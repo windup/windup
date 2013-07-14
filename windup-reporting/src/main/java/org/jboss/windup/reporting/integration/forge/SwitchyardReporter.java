a0918a1546baa1d7f51cbf467f40b0285a526b0d
Brad Davis <bradsdavis@gmail.com>
2013-07-14 14:15:16 -0400

    Added in logic to recurse the archive looking for JBoss ESB Configuration.

--- windup-reporting/src/main/java/org/jboss/windup/reporting/integration/forge/SwitchyardReporter.java
+++ windup-reporting/src/main/java/org/jboss/windup/reporting/integration/forge/SwitchyardReporter.java
@@ -8,12 +8,14 @@ import org.apache.commons.logging.LogFactory;
 import org.jboss.windup.metadata.decoration.AbstractDecoration;
 import org.jboss.windup.metadata.decoration.Classification;
 import org.jboss.windup.metadata.type.FileMetadata;
+import org.jboss.windup.metadata.type.XmlMetadata;
 import org.jboss.windup.metadata.type.archive.ArchiveMetadata;
 import org.jboss.windup.reporting.Reporter;
 
 public class SwitchyardReporter implements Reporter {
 	private static final Log LOG = LogFactory.getLog(SwitchyardReporter.class);
-
+	private static final String ESB_DESC = "JBoss ESB Pipeline Configuration";
+	
 	@Override
 	public void process(ArchiveMetadata archive, File reportDirectory) {
 		if (isJBossESB(archive)) {
@@ -23,17 +25,35 @@ public class SwitchyardReporter implements Reporter {
 		}
 	}
 
-	public boolean isJBossESB(ArchiveMetadata archive) {
+	/***
+	 * Recursively checks for the JBoss ESB configuration.    
+	 * @param archive
+	 * @return True if JBoss ESB Configuration is present.
+	 */
+	protected boolean isJBossESB(ArchiveMetadata archive) {
 		for (FileMetadata entry : archive.getEntries()) {
-			for (AbstractDecoration dec : entry.getDecorations()) {
-				if (dec instanceof Classification) {
-					if (StringUtils.equals(dec.getDescription(),
-							"JBoss ESB Pipeline Configuration")) {
-						return true;
+			//only look for decorations if it is an XML file.
+			if(entry instanceof XmlMetadata) {
+				for (AbstractDecoration dec : entry.getDecorations()) {
+					if (dec instanceof Classification) {
+						if (StringUtils.equals(dec.getDescription(), ESB_DESC)) {
+							return true;
+						}
 					}
 				}
 			}
 		}
+		
+
+		//recurse.
+		if(archive.getNestedArchives() != null) {
+			for(ArchiveMetadata ar : archive.getNestedArchives()) {
+				if(isJBossESB(ar)) {
+					return true;
+				}
+			}
+		}
+		
 		return false;
 	}
 
