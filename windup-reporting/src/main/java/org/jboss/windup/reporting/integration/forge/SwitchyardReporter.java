c30df4fccc264c94369586c7426da48e0d88653d
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2013-07-14 11:21:27 -0400

    Furnace lifecycle

--- windup-reporting/src/main/java/org/jboss/windup/reporting/integration/forge/SwitchyardReporter.java
+++ windup-reporting/src/main/java/org/jboss/windup/reporting/integration/forge/SwitchyardReporter.java
@@ -4,20 +4,50 @@ import java.io.File;
 import java.io.IOException;
 
 import org.apache.commons.io.FileUtils;
+import org.jboss.forge.furnace.Furnace;
+import org.jboss.forge.furnace.repositories.AddonRepositoryMode;
+import org.jboss.forge.furnace.se.FurnaceFactory;
+import org.jboss.forge.furnace.util.OperatingSystemUtils;
 import org.jboss.windup.metadata.type.archive.ArchiveMetadata;
 import org.jboss.windup.reporting.Reporter;
 
-public class SwitchyardReporter implements Reporter {
+public class SwitchyardReporter implements Reporter
+{
 
-	@Override
-	public void process(ArchiveMetadata archive, File reportDirectory) {
-		File forgeOutput = new File(reportDirectory, "forge");
-		try {
-			FileUtils.forceMkdir(forgeOutput);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+   @Override
+   public void process(ArchiveMetadata archive, File reportDirectory)
+   {
+      File forgeOutput = new File(reportDirectory, "forge");
+      try
+      {
+         FileUtils.forceMkdir(forgeOutput);
+
+         final Furnace furnace;
+
+         furnace = FurnaceFactory.getInstance();
+         furnace.addRepository(AddonRepositoryMode.MUTABLE, new File(OperatingSystemUtils.getUserHomeDir(), ".windup"));
+         furnace.startAsync();
+
+         while (!furnace.getStatus().isStarted())
+         {
+            System.out.println("FURNACE STATUS: " + furnace.getStatus());
+            Thread.sleep(100);
+         }
+
+         furnace.stop();
+         System.out.println("Furnace stopped.");
+
+      }
+      catch (IOException e)
+      {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+      catch (InterruptedException e)
+      {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+   }
 
 }
