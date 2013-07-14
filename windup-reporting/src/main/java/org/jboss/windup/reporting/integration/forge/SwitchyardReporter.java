383de0d7c61628737369a187ac65ddf60f6cd07a
Brad Davis <bradsdavis@gmail.com>
2013-07-14 14:07:19 -0400

    Added logic for checking to see if the archive is Java.

--- windup-reporting/src/main/java/org/jboss/windup/reporting/integration/forge/SwitchyardReporter.java
+++ windup-reporting/src/main/java/org/jboss/windup/reporting/integration/forge/SwitchyardReporter.java
@@ -2,16 +2,39 @@ package org.jboss.windup.reporting.integration.forge;
 
 import java.io.File;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.jboss.windup.metadata.decoration.AbstractDecoration;
+import org.jboss.windup.metadata.decoration.Classification;
+import org.jboss.windup.metadata.type.FileMetadata;
 import org.jboss.windup.metadata.type.archive.ArchiveMetadata;
 import org.jboss.windup.reporting.Reporter;
 
-public class SwitchyardReporter implements Reporter
-{
-   @Override
-   public void process(ArchiveMetadata archive, File reportDirectory)
-   {
-	   SwitchyardForgeController sfc = new SwitchyardForgeController();
-	   sfc.process(archive, reportDirectory);
-   }
+public class SwitchyardReporter implements Reporter {
+	private static final Log LOG = LogFactory.getLog(SwitchyardReporter.class);
+
+	@Override
+	public void process(ArchiveMetadata archive, File reportDirectory) {
+		if (isJBossESB(archive)) {
+			LOG.info("Generating Switchyard compontents leveraging JBoss Forge.");
+			SwitchyardForgeController sfc = new SwitchyardForgeController();
+			sfc.process(archive, reportDirectory);
+		}
+	}
+
+	public boolean isJBossESB(ArchiveMetadata archive) {
+		for (FileMetadata entry : archive.getEntries()) {
+			for (AbstractDecoration dec : entry.getDecorations()) {
+				if (dec instanceof Classification) {
+					if (StringUtils.equals(dec.getDescription(),
+							"JBoss ESB Pipeline Configuration")) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
 
 }
