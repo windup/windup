58b8479ab86a076436759c487e72a7d522d9415d
Samuel Tauil <samueltauil@gmail.com>
2013-09-06 11:22:51 -0300

    Update jboss-windup.sh
    change the way the script can receive parameters

--- windup-cli/src/main/scripts/jboss-windup.sh
+++ windup-cli/src/main/scripts/jboss-windup.sh
@@ -1,1 +1,2 @@
-java -jar windup-cli.jar -javaPkgs net.example:com.example:example -input /Users/bradsdavis/Projects/migrations/example-folder
+#java -jar windup-cli.jar -javaPkgs net.example:com.example:example -input /Users/bradsdavis/Projects/migrations/example-folder
+java -jar windup-cli.jar $@
