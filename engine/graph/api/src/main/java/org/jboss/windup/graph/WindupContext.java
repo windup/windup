cbe5801849a2443a32bbe59941b2cc464547baa2
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2014-04-01 14:40:54 -0400

    Avoid initializing directories in proxies

--- engine/graph/api/src/main/java/org/jboss/windup/graph/WindupContext.java
+++ engine/graph/api/src/main/java/org/jboss/windup/graph/WindupContext.java
@@ -7,29 +7,33 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Named;
 
 import org.apache.commons.io.FileUtils;
-import org.jboss.windup.graph.GraphContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Named("windup-context")
 @ApplicationScoped
-public class WindupContext {
+public class WindupContext
+{
+   private static final Logger LOG = LoggerFactory.getLogger(WindupContext.class);
 
-	private static final Logger LOG = LoggerFactory.getLogger(WindupContext.class);
-	
-	private final File runDirectory;
-	private final GraphContext graphContext;
-	
-	public WindupContext() {
-		runDirectory = new File(FileUtils.getTempDirectory(), UUID.randomUUID().toString());
-		graphContext = new GraphContext(new File(runDirectory, "windup-graph"));
-	}
-	
-	public GraphContext getGraphContext() {
-		return graphContext;
-	}
-	
-	public File getRunDirectory() {
-		return runDirectory;
-	}
+   private File runDirectory;
+   private GraphContext graphContext;
+
+   public GraphContext getGraphContext()
+   {
+      if (graphContext == null)
+      {
+         graphContext = new GraphContext(new File(getRunDirectory(), "windup-graph"));
+      }
+      return graphContext;
+   }
+
+   public File getRunDirectory()
+   {
+      if (runDirectory == null)
+      {
+         runDirectory = new File(FileUtils.getTempDirectory(), UUID.randomUUID().toString());
+      }
+      return runDirectory;
+   }
 }
