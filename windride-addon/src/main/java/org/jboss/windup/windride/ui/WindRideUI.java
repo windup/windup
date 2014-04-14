1788440273279b7577f0022688592c8f3368220a
OndraZizka <zizka@seznam.cz>
2014-04-14 16:16:33 +0200

    Fix the forge metadata; Add reportDir input and a message for the user where to find the report.

--- windride-addon/src/main/java/org/jboss/windup/windride/ui/WindRideUI.java
+++ windride-addon/src/main/java/org/jboss/windup/windride/ui/WindRideUI.java
@@ -1,6 +1,7 @@
 package org.jboss.windup.windride.ui;
 
 
+import java.io.File;
 import javax.inject.Inject;
 import org.jboss.forge.addon.resource.DirectoryResource;
 import org.jboss.forge.addon.ui.command.UICommand;
@@ -22,7 +23,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- *
+ *  UI for WindRide Forge addon.
+ * 
  *  @author Ondrej Zizka, ozizka at redhat.com
  */
 public class WindRideUI implements UICommand {
@@ -31,21 +33,30 @@ public class WindRideUI implements UICommand {
 
     @Inject private WindRideService windride;
 
-    // Source server - e.g. directory of EAP 5.
-    @Inject  @WithAttributes(label = "Source server directory", required = true)
-    private UIInput<DirectoryResource> srcServer;
-
-    // Target server, e.g. AS 7. Used to start the server through Arquillian.
-    @Inject  @WithAttributes(label = "Target server directory", required = true)
-    private UIInput<DirectoryResource> destServer;
     private Configuration conf;
+    
+    
+    // --- UI elements ---
+
+    /** Source server - e.g. directory of EAP 5. */
+    @Inject  @WithAttributes(label = "Source server directory", required = true)
+    private UIInput<DirectoryResource> srcServerUI;
+
+    /** Target server, e.g. AS 7. Used to start the server through Arquillian. */
+    @Inject  @WithAttributes(label = "Target server directory", required = true)
+    private UIInput<DirectoryResource> destServerUI;
+
+    /** Directory to store the reports to. */
+    @Inject  @WithAttributes(label = "Directory to store the reports to", required = false)
+    private UIInput<DirectoryResource> reportDirUI;
+
 
 
     // Give Forge the command metadata.
     @Override public UICommandMetadata getMetadata( UIContext uic ) {
-        return Metadata.forCommand(getClass()).name("Run Windup 1.x")
-              .description("Run Windup 1.x Migration Analyzer")
-              .category(Categories.create("Platform", "Migration"));
+        return Metadata.forCommand(getClass()).name("Run WindRide 1.x")
+              .description("Run WindRide 1.x server configuration migrator")
+              .category(Categories.create("Platform", "Migration", "Configuration"));
     }
 
 
@@ -55,31 +66,49 @@ public class WindRideUI implements UICommand {
 
 
     @Override public void initializeUI(final UIBuilder builder) throws Exception {
-         builder.add(srcServer).add(destServer);
+        builder.add(srcServerUI).add(destServerUI).add( reportDirUI );
     }
 
 
+    /**
+     *  Validate user input, and along the way, create the config object which will be used in execute().
+     */
     public void validate( UIValidationContext uivc ) {
         Configuration conf = new Configuration();
         
-        conf.getGlobal().getSourceServerConf().setDir( srcServer.getValue().getContents() );
+        conf.getGlobal().getSourceServerConf().setDir( srcServerUI.getValue().getContents() );
         conf.getGlobal().getSourceServerConf().setProfileName("all");
         
-        conf.getGlobal().getTargetServerConf().setDir( srcServer.getValue().getContents() ); // target/as7copy
+        conf.getGlobal().getTargetServerConf().setDir( srcServerUI.getValue().getContents() ); // target/as7copy
         conf.getGlobal().getTargetServerConf().setConfigPath("standalone/configuration/standalone.xml");
+        
+        // If the user didn't choose reports dir, generate them into <CWD>/WindRide-report-<timestamp> .
+        String reportDir = null;
+        if( this.reportDirUI.hasValue() )
+            reportDir = this.reportDirUI.getValue().getContents();
+        else
+            reportDir = System.getProperty("user.dir") + File.separator + "WindRide-report-" + System.currentTimeMillis();
+            
+        conf.getGlobal().setReportDir( reportDir );
         
         ConfigurationValidator.validate( conf );
         this.conf = conf;
     }
 
 
+    /**
+     *  Performs a migration as per configuration provided by the user.
+     */
     public Result execute( UIExecutionContext uiec ) {
         try {
-            windride.doMigration( conf );
-            return Results.success();
-        } catch( Exception e ) {
-            log.error( "Error executing WindRide", e );
-            return Results.fail( "Error executing WindRide", e );
+            windride.doMigration( this.conf );
+            String msg = "Server config migration report stored in: " + this.conf.getGlobal().getReportDir();
+            return Results.success( msg );
+        }
+        catch( Exception e ) {
+            String msg = "Error executing WindRide: " + e.getMessage();
+            log.error( msg, e );
+            return Results.fail( msg, e );
         }
     }
     
