08e2adbda70c3938483eb72fa5eb7286555209ff
OndraZizka <zizka@seznam.cz>
2014-04-30 02:18:51 +0200

    Fix BasicVisitor to use test file. TODO Needs to be moved to tests later.

--- engine/rules/impl/src/main/java/org/jboss/windup/engine/visitor/inspector/BasicVisitor.java
+++ engine/rules/impl/src/main/java/org/jboss/windup/engine/visitor/inspector/BasicVisitor.java
@@ -1,9 +1,7 @@
 package org.jboss.windup.engine.visitor.inspector;
 
 import java.io.File;
-
 import javax.inject.Inject;
-
 import org.jboss.windup.engine.visitor.AbstractGraphVisitor;
 import org.jboss.windup.engine.visitor.VisitorPhase;
 import org.jboss.windup.graph.dao.FileResourceDao;
@@ -24,11 +22,9 @@ public class BasicVisitor extends AbstractGraphVisitor
     @Override
     public void run()
     {
-        File r1 = new File("/Users/lb3/Desktop/custom-application-remote.war");
+        // Runs likely in <project_root>/engine/tests.
+        // Naive but better than previous hardcoded path to Brad's Desktop :)
+        File r1 = new File("../../test_files/Windup1x-javaee-example.war");
         FileResource r1g = fileDao.createByFilePath(r1.getAbsolutePath());
-        
-        //
-        // File r2 = new File("/Users/bradsdavis/Projects/migrations/inputs/WindupConfigurations.jar");
-        // org.jboss.windup.graph.model.resource.FileResource r2g = fileDao.getByFilePath(r2.getAbsolutePath());
     }
 }
