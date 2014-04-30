fb4593b7231feb94f56019dc05747ee492187a20
OndraZizka <zizka@seznam.cz>
2014-04-30 19:09:57 +0200

    Limit to private, cleanup

--- engine/rules/impl/src/main/java/org/jboss/windup/engine/WindupProcessorImpl.java
+++ engine/rules/impl/src/main/java/org/jboss/windup/engine/WindupProcessorImpl.java
@@ -1,52 +1,49 @@
 package org.jboss.windup.engine;
 
 import java.util.List;
-
 import javax.inject.Inject;
-
 import org.jboss.windup.addon.engine.WindupProcessor;
 import org.jboss.windup.engine.provider.ListenerChainProvider;
 import org.jboss.windup.engine.visitor.GraphVisitor;
 import org.jboss.windup.graph.GraphContext;
-import org.jboss.windup.graph.WindupContext;
-import org.jboss.windup.graph.dao.JavaClassDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ *  The entry point of the engine.
+ */
 public class WindupProcessorImpl implements WindupProcessor
 {
-
     private static final Logger LOG = LoggerFactory.getLogger(WindupProcessorImpl.class);
 
-    @Inject
-    WindupContext windupContext;
     
-    @Inject
-    GraphContext graphContext;
+    //@Inject private WindupContext windupContext;
+    
+    @Inject private GraphContext graphContext;
 
-    @Inject
-    private ListenerChainProvider provider;
+    @Inject private ListenerChainProvider provider;
 
-    @Inject
-    JavaClassDao javaClassDao;
+    //@Inject private JavaClassDao javaClassDao;
 
-    @Inject
-    ConfigurationProcessorImpl configProcessor;
+    @Inject private ConfigurationProcessorImpl configProcessor;
 
+    
+    /**
+     *  The entry point of the engine.
+     */
     @Override
     public void execute()
     {
-        final List<GraphVisitor> listenerChain = provider.getListenerChain();
+        final List<GraphVisitor> visitorChain = this.provider.getListenerChain();
 
-        LOG.info("Executing: " + listenerChain.size() + " listeners...");
-        // LOG.info("Executing: " + listenerChain);
-        for (final GraphVisitor visitor : listenerChain)
+        LOG.info("Executing: " + visitorChain.size() + " listeners...");
+        for (final GraphVisitor visitor : visitorChain)
         {
             LOG.info("Processing: " + visitor + " - Class: " + visitor.getClass());
             visitor.run();
         }
 
-        configProcessor.run(graphContext);
+        this.configProcessor.run(graphContext);
 
         LOG.info("Execution complete.");
     }
