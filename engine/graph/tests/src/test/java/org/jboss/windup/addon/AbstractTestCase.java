867cf5b8cef5839c8aff92e6141a01b5b1587a17
OndraZizka <zizka@seznam.cz>
2014-05-06 19:00:58 +0200

    Reference Test case harness in Graph Tests.

--- engine/graph/tests/src/test/java/org/jboss/windup/addon/AbstractTestCase.java
+++ engine/graph/tests/src/test/java/org/jboss/windup/addon/AbstractTestCase.java
@@ -1,5 +1,39 @@
 package org.jboss.windup.addon;
 
-public abstract class AbstractTestCase
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.forge.arquillian.AddonDependency;
+import org.jboss.forge.arquillian.Dependencies;
+import org.jboss.forge.arquillian.archive.ForgeArchive;
+import org.jboss.forge.furnace.repositories.AddonDependencyEntry;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.junit.runner.RunWith;
+
+
+/**
+ * This is here for reference.
+ * Static getDeployment() doesn't allow leveraging subclassing. FORGE-1790
+ * 
+ * @author Ondrej Zizka, ozizka at redhat.com
+ */
+@RunWith(Arquillian.class)
+public class AbstractTestCase
 {
+
+   @Deployment
+   @Dependencies({
+            //@AddonDependency(name = "org.jboss.windup.rules:rules"),
+            @AddonDependency(name = "org.jboss.forge.furnace.container:cdi")
+   })
+   public static ForgeArchive getDeployment()
+   {
+      ForgeArchive archive = ShrinkWrap.create(ForgeArchive.class)
+        .addBeansXML()
+        .addAsAddonDependencies(
+            //AddonDependencyEntry.create("org.jboss.windup.rules:rules"),
+            AddonDependencyEntry.create("org.jboss.forge.furnace.container:cdi")
+        );
+      return archive;
+   }
+
 }
