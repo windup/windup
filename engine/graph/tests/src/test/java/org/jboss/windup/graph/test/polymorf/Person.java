df506199423990a240273f37b5a901eab7590b9f
OndraZizka <zizka@seznam.cz>
2014-05-06 16:41:57 +0200

    Tinkerpop Frames - Node type change test

---
+++ engine/graph/tests/src/test/java/org/jboss/windup/graph/test/polymorf/Person.java
@@ -0,0 +1,24 @@
+package org.jboss.windup.graph.test.polymorf;
+
+import com.tinkerpop.frames.Adjacency;
+import com.tinkerpop.frames.Property;
+import com.tinkerpop.frames.VertexFrame;
+import com.tinkerpop.frames.modules.typedgraph.TypeField;
+
+/**
+ *
+ * @author Ondrej Zizka, ozizka at redhat.com
+ */
+@TypeField("type")
+public interface Person extends VertexFrame {
+    
+    @Property("name")
+    public String getName();
+
+    @Adjacency(label="parentOf")
+    public Iterable<Person> getParents();  
+
+    @Adjacency(label="parentOf")
+    public void addPerson(final Person person);
+        
+}
---
+++ engine/graph/tests/src/test/java/org/jboss/windup/graph/test/polymorf/SpecialPerson.java
@@ -0,0 +1,12 @@
+package org.jboss.windup.graph.test.polymorf;
+
+import com.tinkerpop.frames.modules.typedgraph.TypeValue;
+
+/**
+ *
+ * @author Ondrej Zizka, ozizka at redhat.com
+ */
+@TypeValue("special")
+public interface SpecialPerson extends Person {
+    
+}
