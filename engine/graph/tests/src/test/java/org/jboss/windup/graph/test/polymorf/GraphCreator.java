6ea42292d07e0cd58728f48eb5b45a3ae8af4aed
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2014-05-06 10:46:04 -0400

    Clean up temp files. OS-independent.

--- engine/graph/tests/src/test/java/org/jboss/windup/graph/test/polymorf/GraphCreator.java
+++ engine/graph/tests/src/test/java/org/jboss/windup/graph/test/polymorf/GraphCreator.java
@@ -1,37 +1,36 @@
 package org.jboss.windup.graph.test.polymorf;
 
+import java.io.File;
 
-import com.tinkerpop.blueprints.Edge;
+import com.thinkaurelius.titan.core.TitanFactory;
+import com.thinkaurelius.titan.core.TitanGraph;
 import com.tinkerpop.blueprints.Graph;
 import com.tinkerpop.blueprints.Vertex;
-import com.tinkerpop.blueprints.impls.tg.TinkerGraph;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
- *  Creates test graphs with some data.
- *  @author Ondrej Zizka, ozizka at redhat.com
+ * Creates test graphs with some data.
+ * 
+ * @author Ondrej Zizka, ozizka at redhat.com
  */
-public class GraphCreator {
-    private static final Logger log = LoggerFactory.getLogger( GraphCreator.class );
+public class GraphCreator
+{
+    public static Graph createFamilyGraph(File dir)
+    {
+        TitanGraph g = TitanFactory.open(dir.getAbsolutePath());
 
-    public static Graph createFamilyGraph(){
-
-        Graph g =  new TinkerGraph();
-        
         // Manipulation through BluePrints API.
-        
+
         // Ja
         Vertex oz = g.addVertex(null);
         oz.setProperty("name", "Ondrej Zizka");
         oz.setProperty("man", true);
-        
+
         // Mamka
         Vertex sz = g.addVertex(null);
         sz.setProperty("name", "Stanislava Zizkova");
         sz.setProperty("man", false);
-        Edge e = g.addEdge( null, sz, oz, "parent");
-        
+        g.addEdge(null, sz, oz, "parent");
+
         // Deda
         Vertex jz0 = g.addVertex(null);
         jz0.setProperty("name", "Josef Zizka");
@@ -41,30 +40,29 @@ public class GraphCreator {
         Vertex bab = g.addVertex(null);
         bab.setProperty("name", "Jaroslava Zizkova");
         bab.setProperty("man", false);
-        g.addEdge( null, jz0, bab, "married");
-        g.addEdge( null, bab, jz0, "married");
+        g.addEdge(null, jz0, bab, "married");
+        g.addEdge(null, bab, jz0, "married");
 
         // Tata
         Vertex jz1 = g.addVertex(null);
         jz1.setProperty("name", "Josef Zizka");
         jz1.setProperty("man", true);
-        g.addEdge( null, jz1, oz, "parent");
-        g.addEdge( null, sz, jz1, "married");
-        g.addEdge( null, jz1, sz, "married");
-        
-        
+        g.addEdge(null, jz1, oz, "parent");
+        g.addEdge(null, sz, jz1, "married");
+        g.addEdge(null, jz1, sz, "married");
+
         // Bracha
         Vertex jz2 = g.addVertex(null);
         jz2.setProperty("name", "Josef Zizka");
         jz2.setProperty("man", true);
-        g.addEdge( null, jz1, jz2, "parent");
-        
+        g.addEdge(null, jz1, jz2, "parent");
+
         // Pepicek
         Vertex jz3 = g.addVertex(null);
         jz3.setProperty("name", "Josef Zizka");
         jz3.setProperty("man", true);
-        g.addEdge( null, jz2, jz3, "parent");
-        
+        g.addEdge(null, jz2, jz3, "parent");
+
         return g;
     }
 
--- engine/graph/tests/src/test/java/org/jboss/windup/graph/test/polymorf/NodeTypeChangeTest.java
+++ engine/graph/tests/src/test/java/org/jboss/windup/graph/test/polymorf/NodeTypeChangeTest.java
@@ -1,5 +1,10 @@
 package org.jboss.windup.graph.test.polymorf;
 
+import java.io.File;
+
+import org.jboss.forge.furnace.impl.util.Files;
+import org.jboss.forge.furnace.util.OperatingSystemUtils;
+import org.junit.Test;
 
 import com.tinkerpop.blueprints.Graph;
 import com.tinkerpop.blueprints.Vertex;
@@ -8,50 +13,58 @@ import com.tinkerpop.frames.FramedGraphFactory;
 import com.tinkerpop.frames.modules.Module;
 import com.tinkerpop.frames.modules.gremlingroovy.GremlinGroovyModule;
 import com.tinkerpop.frames.modules.typedgraph.TypedGraphModuleBuilder;
-import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
- *
- *  @author Ondrej Zizka, ozizka at redhat.com
+ * 
+ * @author Ondrej Zizka, ozizka at redhat.com
  */
-public class NodeTypeChangeTest {
-    private static final Logger log = LoggerFactory.getLogger( NodeTypeChangeTest.class );
-    
-    @Test
-    public void testNodeTypeChange() {
-        Graph g = GraphCreator.createFamilyGraph();
-        
-        Module tgmp = new TypedGraphModuleBuilder()
-                .withClass(SpecialPerson.class) .build();
-        
-        FramedGraphFactory factory = new FramedGraphFactory( new GremlinGroovyModule(), tgmp );
-        FramedGraph framed = factory.create(g);
-        
-        final Vertex v = g.getVertices().iterator().next();
-        
-        // Should be Person.
-        Person foo = (Person) framed.frame( v, Person.class );
-        System.out.println( "Person: " + foo + " " + asString( foo ) );
-        
-        // Try to retype
-        v.setProperty("type", "special");
-        
-        // Should be SpecialPerson.
-        SpecialPerson foo2 = (SpecialPerson) framed.frame( v, Person.class );
-        System.out.println( "SpecialPerson: " + foo2 + " " + asString( foo2 ) );
+public class NodeTypeChangeTest
+{
 
-        // Unknown type - should be Person.
-        v.setProperty("type", "aaaaaa");
-        
-        foo = (Person) framed.frame( v, Person.class );
-        System.out.println( "Not SpecialPerson: " + foo + " " + asString( foo ) );
+    @Test
+    public void testNodeTypeChange()
+    {
+        File graphDir = OperatingSystemUtils.getTempDirectory();
+        try
+        {
+            Graph g = GraphCreator.createFamilyGraph(graphDir);
+
+            Module tgmp = new TypedGraphModuleBuilder()
+                        .withClass(SpecialPerson.class).build();
+
+            FramedGraphFactory factory = new FramedGraphFactory(new GremlinGroovyModule(), tgmp);
+            FramedGraph<Graph> framed = factory.create(g);
+
+            final Vertex v = g.getVertices().iterator().next();
+
+            // Should be Person.
+            Person foo = framed.frame(v, Person.class);
+            System.out.println("Person: " + foo + " " + asString(foo));
+
+            // Try to retype
+            v.setProperty("type", "special");
+
+            // Should be SpecialPerson.
+            SpecialPerson foo2 = (SpecialPerson) framed.frame(v, Person.class);
+            System.out.println("SpecialPerson: " + foo2 + " " + asString(foo2));
+
+            // Unknown type - should be Person.
+            v.setProperty("type", "aaaaaa");
+
+            foo = framed.frame(v, Person.class);
+            System.out.println("Not SpecialPerson: " + foo + " " + asString(foo));
+        }
+        finally
+        {
+            Files.delete(graphDir, true);
+        }
+
     }
-    
-    
-    private static String asString( Person p ){
-        if( p == null ) return "(null)";
+
+    private static String asString(Person p)
+    {
+        if (p == null)
+            return "(null)";
         return "Person name: " + p.getName() + " " + p.getClass().getSimpleName();
     }
 
