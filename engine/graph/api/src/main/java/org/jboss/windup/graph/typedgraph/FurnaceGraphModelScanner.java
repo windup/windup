c6cc0a1b8b976a0d532f07695c705d4f23137278
Jess Sightler <jesse.sightler@gmail.com>
2014-05-06 15:56:35 -0400

    Removed unused import

--- engine/graph/api/src/main/java/org/jboss/windup/graph/typedgraph/FurnaceGraphModelScanner.java
+++ engine/graph/api/src/main/java/org/jboss/windup/graph/typedgraph/FurnaceGraphModelScanner.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
 import java.util.zip.ZipEntry;
-import java.util.zip.ZipException;
 import java.util.zip.ZipFile;
 
 import javax.inject.Inject;
