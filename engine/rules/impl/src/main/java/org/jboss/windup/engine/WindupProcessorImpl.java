68cad026952dc7c7889ed612f731d4c621fb18f0
Jess Sightler <jesse.sightler@gmail.com>
2014-05-14 12:24:41 -0400

    Removed some commented lines

--- engine/rules/impl/src/main/java/org/jboss/windup/engine/WindupProcessorImpl.java
+++ engine/rules/impl/src/main/java/org/jboss/windup/engine/WindupProcessorImpl.java
@@ -1,7 +1,9 @@
 package org.jboss.windup.engine;
 
 import java.util.List;
+
 import javax.inject.Inject;
+
 import org.jboss.windup.addon.engine.WindupProcessor;
 import org.jboss.windup.engine.provider.VisitorChainProvider;
 import org.jboss.windup.engine.visitor.GraphVisitor;
@@ -10,26 +12,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- *  The entry point of the engine.
+ * The entry point of the engine.
  */
 public class WindupProcessorImpl implements WindupProcessor
 {
     private static final Logger LOG = LoggerFactory.getLogger(WindupProcessorImpl.class);
 
-    
-    //@Inject private WindupContext windupContext;
-    
-    @Inject private GraphContext graphContext;
+    @Inject
+    private GraphContext graphContext;
 
-    @Inject private VisitorChainProvider provider;
+    @Inject
+    private VisitorChainProvider provider;
 
-    //@Inject private JavaClassDao javaClassDao;
+    @Inject
+    private ConfigurationProcessorImpl configProcessor;
 
-    @Inject private ConfigurationProcessorImpl configProcessor;
-
-    
     /**
-     *  The entry point of the engine.
+     * The entry point of the engine.
      */
     @Override
     public void execute()
