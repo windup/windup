414628ce8d41b547ba27c598396940d652fbf0c7
Jess Sightler <jesse.sightler@gmail.com>
2014-05-15 10:19:50 -0400

    Reformatted comment

--- engine/graph/addon/src/main/java/org/jboss/windup/addon/package-info.java
+++ engine/graph/addon/src/main/java/org/jboss/windup/addon/package-info.java
@@ -1,7 +1,6 @@
 package org.jboss.windup.addon;
 
 /*
-This keeps various plugins in Maven from complaining about creating empty archives 
-or processing empty archives during the release process.
-It's possible there is another workaround, but I don't remember the exact issues.
+This keeps various plugins in Maven from complaining about creating empty 
+archives or processing empty archives during the release process.
 */
