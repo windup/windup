001bbad2a13aafb429f6bbafeb2af6cfe22bef1a
Jess Sightler <jesse.sightler@gmail.com>
2014-05-16 17:07:10 -0400

    Removed debugging code

--- engine/rules/impl/src/main/java/org/jboss/windup/engine/provider/UnzipArchivesToTempConfigurationProvider.java
+++ engine/rules/impl/src/main/java/org/jboss/windup/engine/provider/UnzipArchivesToTempConfigurationProvider.java
@@ -2,21 +2,16 @@ package org.jboss.windup.engine.provider;
 
 import java.util.List;
 
-import org.jboss.windup.addon.config.GraphRewrite;
 import org.jboss.windup.addon.config.RulePhase;
 import org.jboss.windup.addon.config.WindupConfigurationProvider;
 import org.jboss.windup.addon.config.graphsearch.GraphSearchConditionBuilder;
-import org.jboss.windup.addon.config.operation.GraphOperation;
 import org.jboss.windup.addon.config.operation.Iteration;
 import org.jboss.windup.addon.config.operation.ruleelement.UnzipArchiveToTemporaryFolder;
-import org.jboss.windup.addon.config.selectables.SelectionFactory;
 import org.jboss.windup.graph.GraphContext;
 import org.jboss.windup.graph.model.ArchiveModel;
-import org.jboss.windup.graph.model.meta.WindupVertexFrame;
 import org.jboss.windup.graph.model.resource.FileResourceModel;
 import org.ocpsoft.rewrite.config.Configuration;
 import org.ocpsoft.rewrite.config.ConfigurationBuilder;
-import org.ocpsoft.rewrite.context.EvaluationContext;
 
 public class UnzipArchivesToTempConfigurationProvider extends WindupConfigurationProvider
 {
@@ -47,27 +42,6 @@ public class UnzipArchivesToTempConfigurationProvider extends WindupConfiguratio
                                             .perform(
                                                         UnzipArchiveToTemporaryFolder.unzip("archive")
                                             ).endIteration()
-                    )
-                    .addRule()
-                    .when(GraphSearchConditionBuilder.create("allTheThings"))
-                    .perform(
-                                Iteration.over("allTheThings").var(WindupVertexFrame.class, "thing")
-                                            .perform(new GraphOperation()
-                                            {
-
-                                                @Override
-                                                public void perform(GraphRewrite event, EvaluationContext context)
-                                                {
-                                                    SelectionFactory sel = SelectionFactory.instance(event);
-                                                    WindupVertexFrame frame = sel.getCurrentPayload(
-                                                                WindupVertexFrame.class, "thing");
-
-                                                    System.out.println("TADADADA: " + frame.toPrettyString());
-
-                                                }
-                                            }
-                                            ).endIteration()
                     );
-
     }
 }
