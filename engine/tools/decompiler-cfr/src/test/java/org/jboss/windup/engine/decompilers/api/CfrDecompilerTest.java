3f135935c26e0b99dbaed267f35580cc786dbd75
Jess Sightler <jesse.sightler@gmail.com>
2014-05-16 23:08:11 -0400

    Fixed base class name.

--- engine/tools/decompiler-cfr/src/test/java/org/jboss/windup/engine/decompilers/api/CfrDecompilerTest.java
+++ engine/tools/decompiler-cfr/src/test/java/org/jboss/windup/engine/decompilers/api/CfrDecompilerTest.java
@@ -6,27 +6,29 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- *
+ * 
  * @author Ondrej Zizka, ozizka at redhat.com
  */
-public class CfrDecompilerTest extends IDecompilerTestBase {
-    private static final Logger log = LoggerFactory.getLogger( CfrDecompilerTest.class );
-    
+public class CfrDecompilerTest extends DecompilerTestBase
+{
+    private static final Logger log = LoggerFactory.getLogger(CfrDecompilerTest.class);
+
     @Override
-    protected DecompilationConf createConf() {
+    protected DecompilationConf createConf()
+    {
         return new CfrConf();
     }
 
-
     @Override
-    protected IDecompiler.Jar getDecompiler() {
+    protected IDecompiler.Jar getDecompiler()
+    {
         return new CfrDecompiler();
     }
 
-
     @Override
-    protected boolean isTestFailedOverExpectations( JarDecompilationResults res ) {
-        return ( res.getFailed().size() > 50 );
+    protected boolean isTestFailedOverExpectations(JarDecompilationResults res)
+    {
+        return (res.getFailed().size() > 50);
     }
 
 }// class
