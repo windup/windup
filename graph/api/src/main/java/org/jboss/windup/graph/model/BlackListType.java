9c0830d9a34eca83725d4dfbea3ce477c030eb9d
Jess Sightler <jesse.sightler@gmail.com>
2014-06-12 17:43:52 -0400

    Added blacklist model interfaces

---
+++ graph/api/src/main/java/org/jboss/windup/graph/model/BlackListType.java
@@ -0,0 +1,13 @@
+package org.jboss.windup.graph.model;
+
+public enum BlackListType
+{
+    IMPORT,
+    METHOD_CALL,
+    EXTENDS_TYPE,
+    IMPLEMENTS_TYPE,
+    REFERENCES_EXCEPTION_OF_TYPE,
+    DECLARES_FIELD_OF_TYPE,
+    DECLARES_VARIABLE_OF_TYPE,
+    OTHER
+}
