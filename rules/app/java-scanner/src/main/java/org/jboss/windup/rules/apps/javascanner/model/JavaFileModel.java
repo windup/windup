d4e1413756ecda5ec13ea0321a808efb827de64d
OndraZizka <zizka@seznam.cz>
2014-06-16 18:26:36 +0200

    Add JavaFileModel

---
+++ rules/app/java-scanner/src/main/java/org/jboss/windup/rules/apps/javascanner/model/JavaFileModel.java
@@ -0,0 +1,37 @@
+package org.jboss.windup.rules.apps.javascanner.model;
+
+import com.tinkerpop.frames.Property;
+import com.tinkerpop.frames.modules.typedgraph.TypeValue;
+import org.jboss.windup.graph.model.resource.FileModel;
+import org.jboss.windup.graph.renderer.Label;
+
+/**
+ *  This is good for searches for Java files, which will be frequent.
+ */
+@TypeValue("JavaFile")
+public interface JavaFileModel extends FileModel
+{
+    public static final String PROPERTY_QUALIFIED_NAME = "qualifiedName";
+    public static final String PROPERTY_CLASS_NAME = "className";
+    public static final String PROPERTY_PACKAGE_NAME = "packageName";
+
+    @Label
+    @Property(PROPERTY_QUALIFIED_NAME)
+    public String getQualifiedName();
+
+    @Property(PROPERTY_QUALIFIED_NAME)
+    public void setQualifiedName(String qualifiedName);
+
+    @Property(PROPERTY_PACKAGE_NAME)
+    public String getPackageName();
+
+    @Property(PROPERTY_PACKAGE_NAME)
+    public void setPackageName(String packageName);
+
+    @Property(PROPERTY_CLASS_NAME)
+    public void setClassName(String className);
+
+    @Property(PROPERTY_CLASS_NAME)
+    public String getClassName();
+    
+}
