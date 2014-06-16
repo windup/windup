e6535ed5fa34300727ce2f593b81836c3b272fe1
OndraZizka <zizka@seznam.cz>
2014-06-16 18:05:32 +0200

    RegexMatcherRule

---
+++ rules/app/java-basic-analysis/src/main/java/org/jboss/windup/rules/apps/javabasic/RegexMatcherRule.java
@@ -0,0 +1,113 @@
+package org.jboss.windup.rules.apps.javabasic;
+
+import org.jboss.windup.rules.apps.javabasic.model.RegexModel;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.apache.commons.io.FileUtils;
+import org.jboss.windup.config.GraphRewrite;
+import org.jboss.windup.config.GraphSubset;
+
+import org.jboss.windup.config.RulePhase;
+import org.jboss.windup.config.WindupConfigurationProvider;
+import org.jboss.windup.config.graphsearch.GraphSearchConditionBuilder;
+import org.jboss.windup.config.graphsearch.GraphSearchConditionBuilderGremlin;
+import org.jboss.windup.config.operation.Iteration;
+import org.jboss.windup.config.operation.ruleelement.AbstractIterationOperator;
+import org.jboss.windup.config.selectables.SelectionFactory;
+import org.jboss.windup.graph.GraphContext;
+import org.jboss.windup.graph.model.resource.JavaClassModel;
+import org.jboss.windup.rules.apps.javascanner.provider.DiscoverJavaFilesConfigurationProvider;
+import org.jboss.windup.util.exception.WindupException;
+import org.ocpsoft.rewrite.config.Configuration;
+import org.ocpsoft.rewrite.config.ConfigurationBuilder;
+import org.ocpsoft.rewrite.context.EvaluationContext;
+
+public class RegexMatcherRule extends WindupConfigurationProvider
+{
+    @Override public RulePhase getPhase(){
+        return RulePhase.DISCOVERY;
+    }
+
+    @Override
+    public List<Class<? extends WindupConfigurationProvider>> getClassDependencies()
+    {
+        return generateDependencies(DiscoverJavaFilesConfigurationProvider.class);
+    }
+
+    @Override
+    public Configuration getConfiguration(GraphContext context)
+    {
+        return ConfigurationBuilder.begin()
+            .addRule()
+            .when(
+                GraphSearchConditionBuilderGremlin.create("javaFiles", new ArrayList())
+                .V().framedType( JavaClassModel.class ).has("analyze")
+            )
+            .perform(
+                Iteration.over("javaFiles").var("javaFile")
+                    .perform(
+                        // A nested rule.
+                        GraphSubset.evaluate(
+                            ConfigurationBuilder.begin().addRule()
+                                .when(
+                                    GraphSearchConditionBuilder.create("regexes")
+                                        .ofType(RegexModel.class)
+                                        .withProperty( RegexModel.FOR_LANG, "java")
+                                )
+                                .perform(
+                                    Iteration.over("regexes").var(RegexModel.class, "regex")
+                                    .perform(
+                                        new AbstractIterationOperator<RegexModel>( RegexModel.class, "regex") {
+                                            @Override
+                                            public void perform( GraphRewrite event, EvaluationContext context, RegexModel regex ) {
+                                                
+                                                SelectionFactory sf = SelectionFactory.instance(event);
+                                                JavaClassModel javaFile = sf.getCurrentPayload( JavaClassModel.class, "javaFile");
+                                                
+                                                getRegexMatches( javaFile.getFilePath(), regex.getRegex() );
+                                            }
+                                        }
+                                    )
+                                    .endIteration()
+                                )// perform()
+                        )
+                    )
+                .endIteration()
+            );
+    }
+
+
+
+    /**
+     *  Returns all regex matches in the given file.
+     *  TODO: Perhaps keep the compiled regex'es.
+     */
+    private void getRegexMatches( String filePath, String regex ) {
+        File file = new File(filePath);
+        if( ! file.exists() )
+            throw new WindupException("File doesn't exist: " + filePath);
+            
+        String src;
+        try {
+            src = FileUtils.readFileToString( file, getEncoding());
+        } catch( IOException ex ) {
+            throw new WindupException("Error reading: " + file + " " + ex.getMessage(), ex);
+        }
+
+        try {
+            Pattern pat = Pattern.compile( regex );
+            Matcher mat = pat.matcher( src );
+        } catch( Exception ex ) {
+            throw new WindupException("Error when matching regex: " + ex.getMessage(), ex);
+        }
+    }
+    
+    private static String getEncoding() {
+        return "UTF-8";
+    }
+
+}
---
+++ rules/app/java-basic-analysis/src/main/java/org/jboss/windup/rules/apps/javabasic/model/RegexModel.java
@@ -0,0 +1,41 @@
+package org.jboss.windup.rules.apps.javabasic.model;
+
+import org.jboss.windup.graph.renderer.Label;
+import com.tinkerpop.frames.Property;
+import com.tinkerpop.frames.modules.typedgraph.TypeValue;
+import org.jboss.windup.graph.model.WindupVertexFrame;
+
+
+@TypeValue("Regex")
+public interface RegexModel extends WindupVertexFrame
+{
+    public static final String FOR_LANG = "forLang";
+
+    @Property("regex")
+    public String getRegex();
+
+    @Property("regex")
+    public void setRegex(String regex);
+
+    
+    @Property(FOR_LANG)
+    public String getForLang();
+
+    @Property(FOR_LANG)
+    public void setForLang(String regex);
+
+
+    @Label
+    @Property("desc")
+    public String getDescription();
+
+    @Property("desc")
+    public void setDescription(String desc);
+
+    
+    //@Adjacency(label = "fromRule", direction = Direction.OUT)
+    //public RuleModel getFromRule();
+    
+    //@Adjacency(label = "fromRule", direction = Direction.OUT)
+    //public void setFromRule();
+}
