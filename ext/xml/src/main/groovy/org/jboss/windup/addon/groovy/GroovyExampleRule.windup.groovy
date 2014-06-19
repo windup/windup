f5d75f77944d17bd475ab01f18900520555c7a3d
OndraZizka <zizka@seznam.cz>
2014-06-19 16:29:34 +0200

    Xml config split to an extension, stage 01

---
+++ ext/xml/src/main/groovy/org/jboss/windup/addon/groovy/GroovyExampleRule.windup.groovy
@@ -0,0 +1,27 @@
+import org.ocpsoft.rewrite.context.EvaluationContext;
+import org.jboss.windup.config.operation.GraphOperation;
+import org.jboss.windup.config.GraphRewrite;
+import org.jboss.windup.config.RulePhase;
+import org.jboss.windup.config.graphsearch.GraphSearchConditionBuilder;
+import org.jboss.windup.config.operation.Iteration;
+import org.jboss.windup.graph.model.JavaClassModel;
+
+registerRegexBlackList("sampleRegexBlackListRule-001", "org.apache.wicket.util.string.CssUtils", "You shouldn't do that")
+
+buildWindupRule("ExampleBlacklistRule")
+    .setPhase(RulePhase.MIGRATION_RULES)
+    .addRule()
+    .when(
+        GraphSearchConditionBuilder.create("javaClasses")
+            .ofType(JavaClassModel.class)
+    )
+    .perform(
+        Iteration.over("javaClasses").var("javaClass").perform(
+            new GraphOperation  () {
+                public void perform(GraphRewrite event, EvaluationContext context) {
+                    System.out.println("Performing rewrite operation")
+                }
+            }
+        )
+    )
+    
