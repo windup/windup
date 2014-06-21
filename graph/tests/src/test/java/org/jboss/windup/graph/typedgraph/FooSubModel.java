46ca5833b3d387aac49d0ec0d5bb775043d82a4c
OndraZizka <zizka@seznam.cz>
2014-06-21 15:16:07 +0200

    Make the GraphTypeManagerTest independent of real types.

---
+++ graph/tests/src/test/java/org/jboss/windup/graph/typedgraph/FooSubModel.java
@@ -0,0 +1,15 @@
+package org.jboss.windup.graph.typedgraph;
+
+import com.tinkerpop.frames.modules.typedgraph.TypeValue;
+import org.jboss.windup.graph.model.resource.ResourceModel;
+
+/**
+ *
+ *  @author Ondrej Zizka, ozizka at redhat.com
+ */
+@TypeValue("FooSub")
+interface FooSubModel extends FooModel {
+
+    
+
+}// class
