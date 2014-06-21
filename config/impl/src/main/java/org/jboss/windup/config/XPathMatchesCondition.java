25f447b8bff601f488db2e535fdab7400ff68dab
OndraZizka <zizka@seznam.cz>
2014-06-21 15:21:10 +0200

    Resolve conflicts

--- config/impl/src/main/java/org/jboss/windup/config/XPathMatchesCondition.java
+++ config/impl/src/main/java/org/jboss/windup/config/XPathMatchesCondition.java
@@ -1,27 +1,19 @@
 package org.jboss.windup.config;
 
-import org.jboss.windup.config.GraphRewrite;
-import org.jboss.windup.config.ConfigurationException;
-import javax.inject.Inject;
 import javax.xml.namespace.NamespaceContext;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
-
 import org.jboss.windup.config.condition.GraphCondition;
-import org.jboss.windup.graph.dao.XmlResourceDao;
 import org.jboss.windup.graph.model.resource.XmlResourceModel;
 import org.ocpsoft.rewrite.context.EvaluationContext;
 import org.w3c.dom.Document;
 
+
 public class XPathMatchesCondition extends GraphCondition
 {
-
-    @Inject
-    XmlResourceDao xmlDao;
-
     protected final XPathExpression xpathExpression;
 
     public XPathMatchesCondition(String pattern, NamespaceContext context)
@@ -29,12 +21,10 @@ public class XPathMatchesCondition extends GraphCondition
         final XPathFactory xPathFactory = XPathFactory.newInstance();
         final XPath xpath = xPathFactory.newXPath();
         xpath.setNamespaceContext(context);
-        try
-        {
+        try {
             xpathExpression = xpath.compile(pattern);
         }
-        catch (final XPathExpressionException e)
-        {
+        catch (final XPathExpressionException e) {
             throw new ConfigurationException("Exception parsing the XPath pattern: " + pattern, e);
         }
     }
@@ -42,21 +32,19 @@ public class XPathMatchesCondition extends GraphCondition
     @Override
     public boolean evaluate(GraphRewrite event, EvaluationContext context)
     {
-        if (event.getResource() instanceof XmlResourceModel)
+        if( ! (event.getResource() instanceof XmlResourceModel) )
+            return false;
+        
+        final XmlResourceModel resource = (XmlResourceModel) event.getResource();
+        try
         {
-            final XmlResourceModel resource = (XmlResourceModel) event.getResource();
-            try
-            {
-                final Document document = resource.asDocument();
-                final Boolean result = (Boolean) xpathExpression.evaluate(document, XPathConstants.BOOLEAN);
-                return result;
-            }
-            catch (final Exception e)
-            {
-                throw new RuntimeException("Exception evaluating rule.", e);
-            }
+            final Document document = resource.asDocument();
+            final Boolean result = (Boolean) xpathExpression.evaluate(document, XPathConstants.BOOLEAN);
+            return result;
         }
-        return false;
+        catch (final Exception e)
+        {
+            throw new RuntimeException("Exception evaluating rule.", e);
+        }
     }
-
 }
