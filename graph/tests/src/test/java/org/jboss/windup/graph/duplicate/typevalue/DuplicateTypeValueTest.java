122cc5e8b07ebc067361f37267bebd5bad5660d7
Matej Briskar <mbriskar@redhat.com>
2014-08-01 17:39:45 +0200

    WINDUP-167 (Test-Ignored): Check for duplicate @TypeValues

---
+++ graph/tests/src/test/java/org/jboss/windup/graph/duplicate/typevalue/DuplicateTypeValueTest.java
@@ -0,0 +1,55 @@
+package org.jboss.windup.graph.duplicate.typevalue;
+
+import static org.junit.Assert.fail;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.forge.arquillian.AddonDependency;
+import org.jboss.forge.arquillian.Dependencies;
+import org.jboss.forge.arquillian.archive.ForgeArchive;
+import org.jboss.forge.furnace.repositories.AddonDependencyEntry;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.windup.graph.GraphContext;
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class DuplicateTypeValueTest
+{
+    @Deployment
+    @Dependencies({
+                @AddonDependency(name = "org.jboss.windup.graph:windup-graph"),
+                @AddonDependency(name = "org.jboss.forge.furnace.container:cdi")
+    })
+    public static ForgeArchive getDeployment()
+    {
+        ForgeArchive archive = ShrinkWrap.create(ForgeArchive.class)
+                    .addBeansXML()
+                    .addClasses(TestSimpleModel.class, TestSimpleModel2.class)
+                    .addAsAddonDependencies(
+                                AddonDependencyEntry.create("org.jboss.windup.graph:windup-graph"),
+                                AddonDependencyEntry.create("org.jboss.forge.furnace.container:cdi")
+                    );
+        return archive;
+    }
+
+    @Inject
+    private GraphContext context;
+
+    @Ignore("Until WINDUP-167 is done.")
+    @Test
+    public void testDuplicateTypeValue() throws Exception
+    {
+        Assert.assertNotNull(context);
+
+        TestSimpleModel simpleModel = context.getFramed().addVertex(null, TestSimpleModel.class);
+        TestSimpleModel2 simpleModel2 = context.getFramed().addVertex(null, TestSimpleModel2.class);
+        
+        // test should fail till now
+        fail();
+    }
+}
