fd5666ee517328295ace79a4653ed54cf3a5e434
Jess Sightler <jesse.sightler@gmail.com>
2014-08-07 14:25:18 -0400

    WINDUP-181: Separate ApplicationArchiveModel from ApplicationModel

--- graph/api/src/main/java/org/jboss/windup/graph/model/ApplicationArchiveModel.java
+++ graph/api/src/main/java/org/jboss/windup/graph/model/ApplicationArchiveModel.java
@@ -1,23 +1,8 @@
 package org.jboss.windup.graph.model;
 
-import org.jboss.windup.graph.model.resource.FileModel;
-
-import com.tinkerpop.blueprints.Direction;
-import com.tinkerpop.frames.Adjacency;
 import com.tinkerpop.frames.modules.typedgraph.TypeValue;
 
 @TypeValue("ApplicationArchive")
-public interface ApplicationArchiveModel extends ApplicationModel
+public interface ApplicationArchiveModel extends ApplicationModel, ArchiveModel
 {
-    @Adjacency(label = "archive", direction = Direction.OUT)
-    public void setOriginalArchive(final ArchiveModel module);
-
-    @Adjacency(label = "archive", direction = Direction.OUT)
-    public ArchiveModel getOriginalArchive();
-
-    @Adjacency(label = "directory", direction = Direction.OUT)
-    public FileModel getUnzippedLocation();
-
-    @Adjacency(label = "directory", direction = Direction.OUT)
-    public FileModel setUnzippedLocation(FileModel model);
 }
