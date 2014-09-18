99f1ebc4820be706db1aeb4e4d62616c86cd40da
OndraZizka <zizka@seznam.cz>
2014-09-19 01:46:03 +0200

    WINDUP-301 Skip certain packages when scanning for *Model classes.

--- graph/api/src/main/java/org/jboss/windup/graph/GraphTypeRegistry.java
+++ graph/api/src/main/java/org/jboss/windup/graph/GraphTypeRegistry.java
@@ -1,27 +1,25 @@
 package org.jboss.windup.graph;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Set;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import javax.annotation.PostConstruct;
-import javax.inject.Inject;
-import javax.inject.Singleton;
-
-import org.jboss.forge.furnace.util.OperatingSystemUtils;
-import org.jboss.windup.graph.model.WindupVertexFrame;
-import org.jboss.windup.util.furnace.FurnaceClasspathScanner;
-import org.jboss.windup.util.furnace.FurnaceScannerFilenameFilter;
-
 import com.tinkerpop.blueprints.Element;
 import com.tinkerpop.frames.FramedGraphConfiguration;
 import com.tinkerpop.frames.VertexFrame;
 import com.tinkerpop.frames.modules.AbstractModule;
 import com.tinkerpop.frames.modules.Module;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import org.jboss.forge.furnace.util.OperatingSystemUtils;
+import org.jboss.windup.graph.model.WindupVertexFrame;
+import org.jboss.windup.util.furnace.FurnaceClasspathScanner;
+import org.jboss.windup.util.furnace.FurnaceScannerFilenameFilter;
 
 @Singleton
 public class GraphTypeRegistry
@@ -45,12 +43,36 @@ public class GraphTypeRegistry
         // Scan for all classes form *Model.class.
         FurnaceScannerFilenameFilter modelClassFilter = new FurnaceScannerFilenameFilter()
         {
-            @Override
-            public boolean accept(String name)
+            // Package prefixes to skip.
+            Set<String> skipPackages = new HashSet();
             {
-                return name.endsWith("Model.class");
+                this.skipPackages.add("org.dom4j.");
+                this.skipPackages.add("groovy.model.");
+                this.skipPackages.add("com.sleepycat.persist.");
+                this.skipPackages.add("org.apache.lucene.");
+                this.skipPackages.add("org.openrdf.model.");
+                this.skipPackages.add("org.apache.bcel.");
+                this.skipPackages.add("org.eclipse.jdt.");
+                this.skipPackages.add("javax.");
+                this.skipPackages.add("org.jboss.weld.");
+                this.skipPackages.add("freemarker.");
+            }
+            
+            @Override
+            public boolean accept(String path)
+            {
+                if(!path.endsWith("Model.class"))
+                    return false;
+                
+                final String clsName = FurnaceClasspathScanner.filepathToClassname(path);
+                for(String pkg : this.skipPackages)
+                    if(clsName.startsWith(pkg))
+                        return false;
+                
+                return true;
             }
         };
+        
 
         Iterable<Class<?>> classes = scanner.scanClasses(modelClassFilter);
 
