6794d177889e5d2660c5afaca6a8556248a82082
Jess Sightler <jesse.sightler@gmail.com>
2014-09-30 13:18:06 -0400

    Format w/ the Eclipse formatting profile

--- exec/api/src/main/java/org/jboss/windup/engine/predicates/RuleProviderWithDependenciesPredicate.java
+++ exec/api/src/main/java/org/jboss/windup/engine/predicates/RuleProviderWithDependenciesPredicate.java
@@ -7,6 +7,7 @@ import org.jboss.windup.config.WindupRuleProvider;
 
 /**
  * Executes only the given rule with all it's dependencies and pre-phases.
+ * 
  * @author mbriskar
  *
  */
@@ -14,32 +15,41 @@ public class RuleProviderWithDependenciesPredicate implements Predicate<WindupRu
 {
 
     private WindupRuleProvider ruleProvider;
-    
-    public RuleProviderWithDependenciesPredicate(Class<? extends WindupRuleProvider> ruleProviderClass) throws InstantiationException, IllegalAccessException {
-        ruleProvider=ruleProviderClass.newInstance();
+
+    public RuleProviderWithDependenciesPredicate(Class<? extends WindupRuleProvider> ruleProviderClass)
+                throws InstantiationException, IllegalAccessException
+    {
+        ruleProvider = ruleProviderClass.newInstance();
     }
-    
+
     @Override
     public boolean accept(WindupRuleProvider type)
     {
         int compareTo = type.getPhase().compareTo(ruleProvider.getPhase());
-        if(compareTo < 0) {
+        if (compareTo < 0)
+        {
             // is in the pre-phase
             return true;
-        } else if(compareTo == 0) {
+        }
+        else if (compareTo == 0)
+        {
             List<Class<? extends WindupRuleProvider>> executeAfter = ruleProvider.getExecuteAfter();
             List<String> executeAfterIDs = ruleProvider.getExecuteAfterIDs();
-            if((executeAfter.contains(type.getClass())) || executeAfterIDs.contains(type.getID())) {
+            if ((executeAfter.contains(type.getClass())) || executeAfterIDs.contains(type.getID()))
+            {
                 // is a dependency and are in the same phase
                 return true;
             }
-            for(Class<? extends WindupRuleProvider> ruleProviderClassAfter : executeAfter) {
-                if(ruleProviderClassAfter.isAssignableFrom(type.getClass())) {
+            for (Class<? extends WindupRuleProvider> ruleProviderClassAfter : executeAfter)
+            {
+                if (ruleProviderClassAfter.isAssignableFrom(type.getClass()))
+                {
                     return true;
                 }
             }
-            if(ruleProvider.getClass().isAssignableFrom(type.getClass())) {
-                 // is the given rule provider
+            if (ruleProvider.getClass().isAssignableFrom(type.getClass()))
+            {
+                // is the given rule provider
                 return true;
             }
         }
