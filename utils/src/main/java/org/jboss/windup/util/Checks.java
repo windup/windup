aeaa17bde06eb991e2fa51c085c89dbdb04c6c01
OndraZizka <zizka@seznam.cz>
2014-10-17 17:00:49 +0200

    Add validation of WindupConfiguration.

--- utils/src/main/java/org/jboss/windup/util/Checks.java
+++ utils/src/main/java/org/jboss/windup/util/Checks.java
@@ -1,9 +1,9 @@
-package org.jboss.windup.decompiler.procyon;
+package org.jboss.windup.util;
 
 import java.io.File;
 
 /**
- * 
+ *
  * @author Ondrej Zizka, ozizka at redhat.com
  */
 public class Checks
@@ -18,8 +18,7 @@ public class Checks
         if (!archive.exists())
             throw new IllegalArgumentException(fileDesc + " does not exist: " + archive.getAbsolutePath());
         if (archive.isDirectory())
-            throw new IllegalArgumentException(fileDesc + " is a directory, expected a file: "
-                        + archive.getPath());
+            throw new IllegalArgumentException(fileDesc + " is a directory, expected a file: " + archive.getPath());
     }
 
     public static void checkDirectoryToBeFilled(File outputDir, String dirDesc) throws IllegalArgumentException
@@ -27,8 +26,7 @@ public class Checks
         if (outputDir == null)
             throw new IllegalArgumentException(dirDesc + " must not be null.");
         if (outputDir.exists() && !outputDir.isDirectory())
-            throw new IllegalArgumentException(dirDesc + " is a file, expected a directory: "
-                        + outputDir.getAbsolutePath());
+            throw new IllegalArgumentException(dirDesc + " is a file, expected a directory: " + outputDir.getAbsolutePath());
     }
 
     public static void checkDirectoryToBeRead(File rootDir, String dirDesc)
@@ -39,6 +37,33 @@ public class Checks
             throw new IllegalArgumentException(dirDesc + " does not exist: " + rootDir.getPath());
         if (!rootDir.isDirectory())
             throw new IllegalArgumentException(dirDesc + " is a file, expected a directory: " + rootDir.getPath());
+    }
+
+
+    /**
+     * Throws if the given file is null, is not a file or directory, or is an empty directory.
+     */
+    public static void checkFileOrDirectoryToBeRead(File fileOrDir, String fileDesc)
+    {
+        if (fileOrDir == null)
+            throw new IllegalArgumentException(fileDesc + " must not be null.");
+        if (!fileOrDir.exists())
+            throw new IllegalArgumentException(fileDesc + " does not exist: " + fileOrDir.getAbsolutePath());
+        if (!(fileOrDir.isDirectory() || fileOrDir.isFile()))
+            throw new IllegalArgumentException(fileDesc + " must be a file or a directory: " + fileOrDir.getPath());
+        if (fileOrDir.isDirectory()){
+            if(fileOrDir.list().length == 0)
+            throw new IllegalArgumentException(fileDesc + " is an empty directory: " + fileOrDir.getPath());
+        }
+    }
+
+
+    /**
+     * Returns true if the iterable contains at least one item.
+     */
+    public static boolean checkNotEmpty(Iterable iterable)
+    {
+        return iterable.iterator().hasNext();
     }
 
 }// class
