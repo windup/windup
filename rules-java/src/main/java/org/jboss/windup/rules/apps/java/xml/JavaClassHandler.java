17b160b6f920d5f33cb0cae45da86317523295d6
OndraZizka <zizka@seznam.cz>
2014-12-09 11:07:27 +0100

    WINDUP-419 Fix error msg in JavaClassHandler

--- rules-java/src/main/java/org/jboss/windup/rules/apps/java/xml/JavaClassHandler.java
+++ rules-java/src/main/java/org/jboss/windup/rules/apps/java/xml/JavaClassHandler.java
@@ -19,36 +19,39 @@ import org.jboss.windup.util.exception.WindupException;
 import org.w3c.dom.Element;
 
 /**
- * 
+ *
  * Represents a {@link JavaClass} {@link Condition}.
- * 
+ *
  * Example:
- * 
+ *
  * <pre>
  * &lt;javaclass type="javax.servlet.http.HttpServletRequest"&gt;
  *         &lt;location&gt;METHOD_PARAMETER&lt;/location&gt;
  * &lt;/javaclass&gt;
  * </pre>
- * 
+ *
  * @author jsightler <jesse.sightler@gmail.com>
  *
  */
-@NamespaceElementHandler(elementName = "javaclass", namespace = "http://windup.jboss.org/v1/xml")
+@NamespaceElementHandler(elementName = JavaClassHandler.ELEM_NAME, namespace = "http://windup.jboss.org/v1/xml")
 public class JavaClassHandler implements ElementHandler<JavaClassBuilderAt>
 {
+    public static final String ELEM_NAME = "javaclass";
+    private static final String ATTR_REFERENCES = "references";
+
 
     @Override
     public JavaClassBuilderAt processElement(ParserContext handlerManager, Element element)
                 throws ConfigurationException
     {
-        String type = $(element).attr("references");
+        String type = $(element).attr(ATTR_REFERENCES);
         String as = $(element).attr("as");
         if(as == null) {
             as = Iteration.DEFAULT_VARIABLE_LIST_STRING;
         }
         if (StringUtils.isBlank(type))
         {
-            throw new WindupException("Error, 'javaclass' element must have a non-empty 'type' attribute");
+            throw new WindupException("The '" + ELEM_NAME + "' element must have a non-empty '" + ATTR_REFERENCES + "' attribute");
         }
 
         List<TypeReferenceLocation> locations = new ArrayList<TypeReferenceLocation>();
@@ -65,7 +68,7 @@ public class JavaClassHandler implements ElementHandler<JavaClassBuilderAt>
         {
             javaClassReferences.inType(namePattern);
         }
-        
+
         JavaClassBuilderAt javaClass = javaClassReferences.at(
                     locations.toArray(new TypeReferenceLocation[locations.size()]));
         javaClass.as(as);
