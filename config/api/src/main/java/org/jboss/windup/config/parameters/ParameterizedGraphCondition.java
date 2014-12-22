c44535d50f1d7272383d48b418edc221222f160e
Jess Sightler <jesse.sightler@gmail.com>
2014-12-21 22:57:03 -0500

    Good fix, or too sleepy too know better? You decide.

--- config/api/src/main/java/org/jboss/windup/config/parameters/ParameterizedGraphCondition.java
+++ config/api/src/main/java/org/jboss/windup/config/parameters/ParameterizedGraphCondition.java
@@ -90,10 +90,10 @@ public abstract class ParameterizedGraphCondition extends GraphCondition impleme
                         Map<String, Iterable<? extends WindupVertexFrame>> layer = entry.getValue();
                         if (layer == null)
                         {
-                            layer = Variables.instance(event).peek();
-                            entry.setValue(layer);
+                            throw new WindupException("Value store with no associated variables frame. This should not happen");
                         }
-                        Iterable<? extends WindupVertexFrame> variable = Variables.instance(event).findVariable(getVarname());
+
+                        Iterable<? extends WindupVertexFrame> variable = layer.get(getVarname());
                         if (variable != null)
                         {
                             for (WindupVertexFrame frame : variable)
@@ -101,7 +101,8 @@ public abstract class ParameterizedGraphCondition extends GraphCondition impleme
                                 ParameterValueStore last = resultSetStores.put(frame, valueStore);
                                 if (last != null)
                                 {
-                                    throw new WindupException(); // YEAH, this happens all the time :)
+                                    // WHY DOES THIS HAPPEN?
+                                    System.out.println("DOES THIS STILL HAPPEN?");
                                 }
                             }
                         }
