87d33b339b5f108e7089b2c11ea88a866f929222
Brad Davis <bradsdavis@gmail.com>
2015-02-03 13:05:03 -0500

    Added in icons to the navbar.

--- rules-java/src/main/java/org/jboss/windup/rules/apps/java/reporting/rules/CreateJavaNonClassifiedFileReportRuleProvider.java
+++ rules-java/src/main/java/org/jboss/windup/rules/apps/java/reporting/rules/CreateJavaNonClassifiedFileReportRuleProvider.java
@@ -71,6 +71,7 @@ public class CreateJavaNonClassifiedFileReportRuleProvider extends WindupRulePro
         applicationReportModel.setReportPriority(200);
         applicationReportModel.setDisplayInApplicationReportIndex(true);
         applicationReportModel.setReportName("Unclassified Files");
+        applicationReportModel.setReportIconClass("glyphicon glyphicon-question-sign");
         applicationReportModel.setMainApplicationReport(false);
         applicationReportModel.setProjectModel(projectModel);
         applicationReportModel.setTemplatePath(TEMPLATE_APPLICATION_REPORT);
