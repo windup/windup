c621a7c900366ace82f8f384fbbdeee7fff9f89b
Jess Sightler <jesse.sightler@gmail.com>
2015-02-12 16:57:28 -0500

    WINDUP-464: Split simple layout projects into addon/tests (or addon/api/impl/tests) layout

---
+++ rules-java/api/src/main/java/org/jboss/windup/rules/apps/java/decompiler/DecompileArchivesRuleProvider.java
@@ -0,0 +1,84 @@
+package org.jboss.windup.rules.apps.java.decompiler;
+
+import org.jboss.windup.config.GraphRewrite;
+import org.jboss.windup.config.WindupRuleProvider;
+import org.jboss.windup.config.operation.Commit;
+import org.jboss.windup.config.operation.IterationProgress;
+import org.jboss.windup.config.operation.Log;
+import org.jboss.windup.config.phase.Decompilation;
+import org.jboss.windup.config.phase.RulePhase;
+import org.jboss.windup.config.query.Query;
+import org.jboss.windup.config.query.QueryGremlinCriterion;
+import org.jboss.windup.graph.GraphContext;
+import org.jboss.windup.graph.model.ArchiveModel;
+import org.jboss.windup.graph.model.resource.FileModel;
+import org.jboss.windup.rules.apps.java.model.JavaClassFileModel;
+import org.jboss.windup.rules.apps.java.service.WindupJavaConfigurationService;
+import org.ocpsoft.logging.Logger.Level;
+import org.ocpsoft.rewrite.config.Configuration;
+import org.ocpsoft.rewrite.config.ConfigurationBuilder;
+
+import com.tinkerpop.blueprints.Vertex;
+import com.tinkerpop.gremlin.java.GremlinPipeline;
+import com.tinkerpop.pipes.PipeFunction;
+
+public class DecompileArchivesRuleProvider extends WindupRuleProvider
+{
+    @Override
+    public Class<? extends RulePhase> getPhase()
+    {
+        return Decompilation.class;
+    }
+
+    // @formatter:off
+    @Override
+    public Configuration getConfiguration(GraphContext context)
+    {
+        QueryGremlinCriterion shouldDecompileCriterion = new ShouldDecompileCriterion();
+        
+        return ConfigurationBuilder.begin()
+        .addRule()
+        .when(Query.fromType(ArchiveModel.class).piped(shouldDecompileCriterion))
+        .perform(
+            new ProcyonDecompilerOperation()
+            .and(IterationProgress.monitoring("Decompiled archive: ", 1))
+            .and(Commit.every(1))
+        )
+        .otherwise(Log.message(Level.WARN, "No archives to decompile."));
+    }
+    // @formatter:on
+
+    /**
+     * A Gremlin criterion that only passes along Vertices with Java Classes that appear to be interesting (in the package list that we are interested
+     * in).
+     */
+    private class ShouldDecompileCriterion implements QueryGremlinCriterion
+    {
+        @Override
+        public void query(final GraphRewrite event, GremlinPipeline<Vertex, Vertex> pipeline)
+        {
+            pipeline.filter(new PipeFunction<Vertex, Boolean>()
+            {
+                @Override
+                public Boolean compute(Vertex argument)
+                {
+                    ArchiveModel archive = event.getGraphContext().getFramed().frame(argument, ArchiveModel.class);
+                    WindupJavaConfigurationService windupJavaConfigurationService = new WindupJavaConfigurationService(
+                                event.getGraphContext());
+                    for (FileModel fileModel : archive.getContainedFileModels())
+                    {
+                        if (fileModel instanceof JavaClassFileModel)
+                        {
+                            JavaClassFileModel javaClassFileModel = (JavaClassFileModel) fileModel;
+                            if (windupJavaConfigurationService.shouldScanPackage(javaClassFileModel.getPackageName()))
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                    return false;
+                }
+            });
+        }
+    }
+}
