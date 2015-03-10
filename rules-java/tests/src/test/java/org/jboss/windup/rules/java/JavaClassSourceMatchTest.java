21a3d9cf6626537e0eff58c9838bb03d700c2f1c
Jess Sightler <jesse.sightler@gmail.com>
2015-03-10 18:54:19 -0400

    Compilation and unit test fix

--- rules-java/tests/src/test/java/org/jboss/windup/rules/java/JavaClassSourceMatchTest.java
+++ rules-java/tests/src/test/java/org/jboss/windup/rules/java/JavaClassSourceMatchTest.java
@@ -5,7 +5,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collections;
-import java.util.List;
 import java.util.logging.Logger;
 
 import javax.inject.Inject;
@@ -21,11 +20,12 @@ import org.jboss.forge.arquillian.archive.ForgeArchive;
 import org.jboss.forge.furnace.repositories.AddonDependencyEntry;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.windup.ast.java.data.TypeReferenceLocation;
+import org.jboss.windup.config.AbstractRuleProvider;
 import org.jboss.windup.config.GraphRewrite;
 import org.jboss.windup.config.RuleSubset;
-import org.jboss.windup.config.WindupRuleProvider;
+import org.jboss.windup.config.metadata.MetadataBuilder;
 import org.jboss.windup.config.operation.Iteration;
-import org.jboss.windup.config.operation.ruleelement.AbstractIterationOperation;
+import org.jboss.windup.config.operation.iteration.AbstractIterationOperation;
 import org.jboss.windup.engine.predicates.RuleProviderWithDependenciesPredicate;
 import org.jboss.windup.exec.WindupProcessor;
 import org.jboss.windup.exec.configuration.WindupConfiguration;
@@ -41,7 +41,6 @@ import org.jboss.windup.rules.apps.java.scan.provider.AnalyzeJavaFilesRuleProvid
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.ocpsoft.rewrite.config.ConditionBuilder;
 import org.ocpsoft.rewrite.config.Configuration;
 import org.ocpsoft.rewrite.config.ConfigurationBuilder;
 import org.ocpsoft.rewrite.context.EvaluationContext;
@@ -157,7 +156,7 @@ public class JavaClassSourceMatchTest
     }
 
     @Singleton
-    public static class JavaClassTestRuleProvider extends WindupRuleProvider
+    public static class JavaClassTestRuleProvider extends AbstractRuleProvider
     {
         private static Logger log = Logger.getLogger(RuleSubset.class.getName());
 
@@ -165,6 +164,11 @@ public class JavaClassSourceMatchTest
         private int firstRuleMatchCount = 0;
         private int secondRuleMatchCount = 0;
         private int fourthRuleMatchCount = 0;
+
+        public JavaClassTestRuleProvider()
+        {
+            super(MetadataBuilder.forProvider(JavaClassTestRuleProvider.class).addExecuteAfter(AnalyzeJavaFilesRuleProvider.class));
+        }
 
         // @formatter:off
         @Override
@@ -236,12 +240,6 @@ public class JavaClassSourceMatchTest
         public int getFourthRuleMatchCount()
         {
             return fourthRuleMatchCount;
-        }
-
-        @Override
-        public List<Class<? extends WindupRuleProvider>> getExecuteAfter()
-        {
-            return asClassList(AnalyzeJavaFilesRuleProvider.class);
         }
     }
 }
