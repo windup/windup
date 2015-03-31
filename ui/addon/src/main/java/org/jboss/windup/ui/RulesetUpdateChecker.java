bd68d8d49b0f4c6ce98b3e246359adf09dad349c
Matej Briskar <mbriskar@redhat.com>
2015-03-31 12:53:34 +0200

    MIGR-198 - TEST RulesetUpdateCommand

--- ui/addon/src/main/java/org/jboss/windup/ui/RulesetUpdateChecker.java
+++ ui/addon/src/main/java/org/jboss/windup/ui/RulesetUpdateChecker.java
@@ -2,8 +2,6 @@ package org.jboss.windup.ui;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
