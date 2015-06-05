4b5020df3417beb5b0378bb8b7916f763813b1bf
Jess Sightler <jesse.sightler@gmail.com>
2015-06-05 12:48:14 -0400

    Make the version attribute optional on source and target technology tags.

--- config-xml/addon/src/main/java/org/jboss/windup/config/parser/metadata/MetadataSourceTechnologyHandler.java
+++ config-xml/addon/src/main/java/org/jboss/windup/config/parser/metadata/MetadataSourceTechnologyHandler.java
@@ -1,46 +1,17 @@
 package org.jboss.windup.config.parser.metadata;
 
-import org.apache.commons.lang.StringUtils;
-import org.jboss.forge.furnace.versions.VersionException;
-import org.jboss.forge.furnace.versions.Versions;
-import org.jboss.windup.config.exception.ConfigurationException;
-import org.jboss.windup.config.metadata.TechnologyReference;
-import org.jboss.windup.config.parser.ElementHandler;
 import org.jboss.windup.config.parser.NamespaceElementHandler;
-import org.jboss.windup.config.parser.ParserContext;
-import org.jboss.windup.config.parser.WindupXMLRulesetParsingException;
 import org.jboss.windup.config.parser.xml.RuleProviderHandler;
-import org.w3c.dom.Element;
 
+/**
+ * Implements a handler for:
+ *
+ * <pre>
+ *     &lt;sourceTechnology id="proprietaryserver" versionRange="(1,12]"/&gt;
+ * </pre>
+ */
 @NamespaceElementHandler(elementName = MetadataSourceTechnologyHandler.METADATA_SOURCE_TECHNOLOGY_ELEMENT, namespace = RuleProviderHandler.WINDUP_RULE_NAMESPACE)
-public class MetadataSourceTechnologyHandler implements ElementHandler<TechnologyReference>
+public class MetadataSourceTechnologyHandler extends AbstractMetadataTechnologyHandler
 {
-
-    private static String ID="id";
-    private static String VERSION_RANGE="versionRange";
     public static final String METADATA_SOURCE_TECHNOLOGY_ELEMENT="sourceTechnology";
-    
-    @Override
-    public TechnologyReference processElement(ParserContext handlerManager, Element element) throws ConfigurationException
-    {
-        String id = element.getAttribute(ID);
-        String versionRange = element.getAttribute(VERSION_RANGE);
-        if (StringUtils.isBlank(id))
-        {
-            throw new WindupXMLRulesetParsingException("The '" + METADATA_SOURCE_TECHNOLOGY_ELEMENT + "' element must have a non-empty '" + ID + "' attribute");
-        }
-        if (StringUtils.isBlank(versionRange))
-        {
-            throw new WindupXMLRulesetParsingException("The '" + METADATA_SOURCE_TECHNOLOGY_ELEMENT + "' element must have a non-empty '" + VERSION_RANGE + "' attribute");
-        }
-        try{
-            Versions.parseVersionRange(versionRange);
-        } catch(VersionException ex) {
-            throw new WindupXMLRulesetParsingException("The '" + VERSION_RANGE + "' attribute with value \"" + versionRange+ "\" in the element " +METADATA_SOURCE_TECHNOLOGY_ELEMENT + " is not a valid version",ex);
-        }
-        
-        TechnologyReference reference = new TechnologyReference(id, versionRange);
-        return reference;
-    }
-    
 }
