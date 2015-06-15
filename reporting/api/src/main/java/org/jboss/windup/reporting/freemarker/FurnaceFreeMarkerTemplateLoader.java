b77f2bec7fbba84bba6d00cb75f190658791b471
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2015-06-15 15:32:16 -0400

    Prevent lookups from stale addons

--- reporting/api/src/main/java/org/jboss/windup/reporting/freemarker/FurnaceFreeMarkerTemplateLoader.java
+++ reporting/api/src/main/java/org/jboss/windup/reporting/freemarker/FurnaceFreeMarkerTemplateLoader.java
@@ -1,9 +1,12 @@
 package org.jboss.windup.reporting.freemarker;
 
 import java.net.URL;
+import java.util.concurrent.Callable;
 
 import org.jboss.forge.furnace.Furnace;
 import org.jboss.forge.furnace.addons.Addon;
+import org.jboss.forge.furnace.lock.LockMode;
+import org.jboss.forge.furnace.util.AddonFilters;
 import org.jboss.windup.config.furnace.FurnaceHolder;
 
 import freemarker.cache.URLTemplateLoader;
@@ -11,19 +14,26 @@ import freemarker.cache.URLTemplateLoader;
 class FurnaceFreeMarkerTemplateLoader extends URLTemplateLoader
 {
     @Override
-    protected URL getURL(String name)
+    protected URL getURL(final String name)
     {
-        URL result = null;
-        Furnace furnace = FurnaceHolder.getFurnace();
-        for (Addon addon : furnace.getAddonRegistry().getAddons())
+        final Furnace furnace = FurnaceHolder.getFurnace();
+        return furnace.getLockManager().performLocked(LockMode.READ, new Callable<URL>()
         {
-            URL url = addon.getClassLoader().getResource(name);
-            if (url != null)
+            @Override
+            public URL call() throws Exception
             {
-                result = url;
-                break;
+                URL result = null;
+                for (Addon addon : furnace.getAddonRegistry().getAddons(AddonFilters.allLoaded()))
+                {
+                    URL url = addon.getClassLoader().getResource(name);
+                    if (url != null)
+                    {
+                        result = url;
+                        break;
+                    }
+                }
+                return result;
             }
-        }
-        return result;
+        });
     }
 }
