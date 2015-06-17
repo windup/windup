0f4bc348c0aa769e119c37b45ac8e9d5faaff35d
Brad Davis <bradsdavis@gmail.com>
2015-06-16 22:08:10 -0400

    WINDUP-649 - Embedded the Tattletale report and provided icon for report consistency.

--- rules-tattletale/addon/src/main/java/org/jboss/windup/rules/apps/tattletale/CreateTattletaleReportLinkRuleProvider.java
+++ rules-tattletale/addon/src/main/java/org/jboss/windup/rules/apps/tattletale/CreateTattletaleReportLinkRuleProvider.java
@@ -1,5 +1,9 @@
 package org.jboss.windup.rules.apps.tattletale;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
 import org.jboss.windup.config.AbstractRuleProvider;
 import org.jboss.windup.config.GraphRewrite;
 import org.jboss.windup.config.metadata.MetadataBuilder;
@@ -13,12 +17,17 @@ import org.jboss.windup.graph.service.WindupConfigurationService;
 import org.jboss.windup.reporting.model.ApplicationReportModel;
 import org.jboss.windup.reporting.model.TemplateType;
 import org.jboss.windup.reporting.service.ApplicationReportService;
+import org.jboss.windup.reporting.service.ReportService;
+import org.jboss.windup.util.Logging;
 import org.ocpsoft.rewrite.config.Configuration;
 import org.ocpsoft.rewrite.config.ConfigurationBuilder;
 import org.ocpsoft.rewrite.context.EvaluationContext;
 
 public class CreateTattletaleReportLinkRuleProvider extends AbstractRuleProvider
 {
+    private static final Logger LOG = Logging.get(CreateTattletaleReportLinkRuleProvider.class);
+    
+    public static final String REPORT_TEMPLATE = "/reports/templates/embedded.ftl";
     private static final String TATTLETALE_INDEX_HTML = "tattletale/index.html";
 
     public CreateTattletaleReportLinkRuleProvider()
@@ -47,10 +56,28 @@ public class CreateTattletaleReportLinkRuleProvider extends AbstractRuleProvider
 
             ApplicationReportModel applicationReportModel = new ApplicationReportService(event.getGraphContext()).create();
             applicationReportModel.setReportName("Tattletale");
+            applicationReportModel.setReportIconClass("glyphicon tattletale-nav-logo");
+            applicationReportModel.setReportPriority(500);
+            
             applicationReportModel.setDisplayInApplicationReportIndex(true);
             applicationReportModel.setProjectModel(inputProjectModel);
-            applicationReportModel.setTemplateType(TemplateType.OTHER);
             applicationReportModel.setReportFilename(TATTLETALE_INDEX_HTML);
+            applicationReportModel.setTemplatePath(REPORT_TEMPLATE);
+            applicationReportModel.setTemplateType(TemplateType.FREEMARKER);
+
+            
+            Map<String, String> reportProperties = new HashMap<>();
+            reportProperties.put("embeddedTitle", "Tattletale Report");
+            reportProperties.put("embeddedUrl", "tattletale/index.html");
+            
+            applicationReportModel.setReportProperties(reportProperties);
+            
+            
+            LOG.info("Title: "+applicationReportModel.getReportProperties().get("embeddedTitle"));
+            LOG.info("URL: "+applicationReportModel.getReportProperties().get("embeddedUrl"));
+            
+            ReportService reportService = new ReportService(event.getGraphContext());
+            reportService.setUniqueFilename(applicationReportModel, "tattletale"+"_"+inputProjectModel.getName(), "html");
         }
     }
 }
