f9ea327fcd95bbdc94890cf4fcb2e9c6456014e2
Marek Novotný <hotmana76@gmail.com>
2015-11-23 16:35:00 +0100

    WINDUP-829 added collapse/expand all link

--- rules-java/api/src/main/resources/reports/resources/js/windup-overview.js
+++ rules-java/api/src/main/resources/reports/resources/js/windup-overview.js
@@ -2,13 +2,33 @@ $(document).on('click', '.panel-heading', function(e){
     var $this = $(this);
 	if(!$this.hasClass('panel-collapsed')) {
 		$this.parents('.panel').find('.panel-body').slideUp();
-        $this.parents('.panel').removeClass('panel-boarding');
+      $this.parents('.panel').removeClass('panel-boarding').addClass('panel-collapsed');
 		$this.addClass('panel-collapsed');
 		$this.find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
 	} else {
 		$this.parents('.panel').find('.panel-body').slideDown();
-        $this.parents('.panel').addClass('panel-boarding');
-		$this.removeClass('panel-collapsed');
+      $this.parents('.panel').addClass('panel-boarding');
+		$this.removeClass('panel-collapsed').addClass('panel-boarding');;
 		$this.find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
 	}
 })
+$('#collapseAll').toggle();
+
+//
+function expandAll(){
+	$('.panel-body').slideDown();
+	$('.panel-heading').find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+	$('.panel-heading').addClass('panel-boarding').removeClass('panel-collapsed');
+	$('.panel-heading').parents('.panel').addClass('panel-boarding');	
+	$('#expandAll').toggle();	
+	$('#collapseAll').toggle();
+}
+
+function collapseAll(){
+	$('.panel-body').slideUp();
+	$('.panel-heading').find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+	$('.panel-heading').addClass('panel-collapsed').removeClass('panel-boarding');
+	$('.panel-heading').parents('.panel').addClass('panel-collapsed').removeClass('panel-boarding');
+	$('#expandAll').toggle();
+	$('#collapseAll').toggle();
+}
