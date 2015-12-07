db9396f2af9e5553ec981fbe58a2d186e5ec180a
Brad Davis <bradsdavis@gmail.com>
2015-12-07 12:05:33 -0500

    WINDUP-863 - Made the static IP Address scanner take into consideration commented out properties and Maven version numbers.

--- rules-java/api/src/main/java/org/jboss/windup/rules/apps/java/ip/DiscoverStaticIPAddressRuleProvider.java
+++ rules-java/api/src/main/java/org/jboss/windup/rules/apps/java/ip/DiscoverStaticIPAddressRuleProvider.java
@@ -1,5 +1,15 @@
 package org.jboss.windup.rules.apps.java.ip;
 
+
+import static org.joox.JOOX.$;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.LineIterator;
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.validator.routines.InetAddressValidator;
 import org.jboss.windup.config.AbstractRuleProvider;
 import org.jboss.windup.config.GraphRewrite;
@@ -8,12 +18,18 @@ import org.jboss.windup.config.operation.iteration.AbstractIterationOperation;
 import org.jboss.windup.config.phase.MigrationRulesPhase;
 import org.jboss.windup.graph.GraphContext;
 import org.jboss.windup.graph.service.GraphService;
+import org.jboss.windup.reporting.model.ClassificationModel;
+import org.jboss.windup.reporting.service.ClassificationService;
+import org.jboss.windup.rules.apps.java.model.PropertiesModel;
+import org.jboss.windup.rules.apps.xml.model.XmlFileModel;
 import org.jboss.windup.rules.files.condition.FileContent;
 import org.jboss.windup.rules.files.model.FileLocationModel;
 import org.ocpsoft.rewrite.config.Configuration;
 import org.ocpsoft.rewrite.config.ConfigurationBuilder;
 import org.ocpsoft.rewrite.config.Rule;
 import org.ocpsoft.rewrite.context.EvaluationContext;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 /**
  * Finds files that contain potential static IP addresses, determined by regular expression.
@@ -22,6 +38,9 @@ import org.ocpsoft.rewrite.context.EvaluationContext;
  */
 public class DiscoverStaticIPAddressRuleProvider extends AbstractRuleProvider
 {
+	private static final String IP_PATTERN = "(?<![\\w.])\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}(?![\\w.])";
+	private static final Logger LOG = Logger.getLogger(DiscoverStaticIPAddressRuleProvider.class.getSimpleName());
+	
     public DiscoverStaticIPAddressRuleProvider()
     {
         super(MetadataBuilder.forProvider(DiscoverStaticIPAddressRuleProvider.class)
@@ -49,7 +68,16 @@ public class DiscoverStaticIPAddressRuleProvider extends AbstractRuleProvider
                             // graph
                             if (InetAddressValidator.getInstance().isValid(payload.getSourceSnippit()))
                             {
-                                StaticIPLocationModel location = GraphService.addTypeToModel(event.getGraphContext(), payload,
+                            	//if the file is a property file, make sure the line isn't commented out.
+                            	
+                        		if(isCommentedPropertyLine(event.getGraphContext(), payload)) {
+                        			return;
+                        		}
+                            	if(isMavenVersionTag(event.getGraphContext(), payload)) {
+                            		return;
+                            	}
+                            	
+                            	StaticIPLocationModel location = GraphService.addTypeToModel(event.getGraphContext(), payload,
                                         StaticIPLocationModel.class);
                                 location.setRuleID(((Rule) context.get(Rule.class)).getId());
                                 location.setTitle("Static IP Address Detected");
@@ -66,8 +94,93 @@ public class DiscoverStaticIPAddressRuleProvider extends AbstractRuleProvider
                             }
                         }
                     })
-                    .where("ip").matches("(?<![\\w.])\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}(?![\\w.])")
+                    .where("ip").matches(IP_PATTERN)
                     .where("type").matches("java|properties|xml")
                     .withId(getClass().getSimpleName());
     }
+    
+    /**
+     * if the line is commented out, returns true
+     * @param context
+     * @param model
+     * @return
+     */
+    private boolean isCommentedPropertyLine(GraphContext context, FileLocationModel model) {
+    	if(model.getFile() instanceof PropertiesModel) {
+			//check to see if the line is commented...
+			int lineNumber = model.getLineNumber();
+			LineIterator li = null;
+			try {
+				li = FileUtils.lineIterator(model.getFile().asFile());
+				
+				int i = 0;
+				while(li.hasNext()) {
+					i++;
+
+					//read the line to memory only if it is the line of interest
+					if(i == lineNumber) {
+						String line = StringUtils.trim(li.next());
+						//check that it isn't commented.
+						if(StringUtils.startsWith(line, "#")) {
+							return true;
+						}
+						return false;
+					}
+					else if(i < lineNumber) {
+						//seek
+						li.next();
+					}
+					else if(i > lineNumber) {
+						LOG.warning("Did not find line: "+lineNumber+" in file: "+model.getFile().getFileName());
+						break;
+					}
+				}
+			} catch (IOException | RuntimeException e) {
+				LOG.log(Level.WARNING, "Exception reading properties from file: " + model.getFile().getFilePath(), e);
+			}
+			finally {
+				LineIterator.closeQuietly(li);
+			}
+			
+    	}
+	
+    	return false;
+    }
+    
+    private boolean isMavenFile(GraphContext context, FileLocationModel model) {
+    	if(!(model.getFile() instanceof XmlFileModel)) {
+    		return false;
+    	}
+    	
+    	ClassificationService cs = new ClassificationService(context);
+    	for(ClassificationModel cm : cs.getClassificationByName(model.getFile(), "Maven POM")) {
+    		return true;
+    	}
+    	return false;
+    }
+    
+
+    /**
+     *  if this is a maven file, checks to see if "version" tags match the discovered text; if the 
+     *  discovered text does match something in a version tag, it is likely a version, not an IP address
+     *  
+     * @param context
+     * @param model
+     * @return
+     */
+    private boolean isMavenVersionTag(GraphContext context, FileLocationModel model) {
+    	if(isMavenFile(context, model)) {
+    		Document doc = ((XmlFileModel)model.getFile()).asDocument();
+    		for(Element elm : $(doc).find("version")) {
+    			String text = StringUtils.trim($(elm).text());
+    			if(StringUtils.equals(text, model.getSourceSnippit())) {
+    				return true;
+    			}
+    		}
+    	}
+    	return false;
+    }
+    
+    
+    
 }
