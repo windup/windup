dfb7955615bfd1dbe9446f7a0f236f2dead1d4bd
Jess Sightler <jesse.sightler@gmail.com>
2015-12-07 19:17:53 -0500

    Fixed a bug with the migration issues link

--- reporting/api/src/main/java/org/jboss/windup/reporting/model/ApplicationReportModel.java
+++ reporting/api/src/main/java/org/jboss/windup/reporting/model/ApplicationReportModel.java
@@ -6,8 +6,11 @@ import org.jboss.windup.graph.MapInAdjacentProperties;
 import org.jboss.windup.graph.model.ProjectModel;
 
 import com.tinkerpop.blueprints.Direction;
+import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.frames.Adjacency;
 import com.tinkerpop.frames.Property;
+import com.tinkerpop.frames.modules.javahandler.JavaHandler;
+import com.tinkerpop.frames.modules.javahandler.JavaHandlerContext;
 import com.tinkerpop.frames.modules.typedgraph.TypeValue;
 
 /**
@@ -36,9 +39,10 @@ public interface ApplicationReportModel extends ReportModel
     void setApplicationReportIndexModel(ApplicationReportIndexModel navIndex);
 
     /**
-     * Provides a link to the Navigation Index that is used for this particular report
+     * Provides a link to the Navigation Index that is used for this particular report. If there is more than one (for example, in the case of a
+     * single report used both globally and associated with an application), then return the one associated with an app.
      */
-    @Adjacency(label = ApplicationReportIndexModel.APPLICATION_REPORT_INDEX_TO_REPORT_MODEL, direction = Direction.IN)
+    @JavaHandler
     ApplicationReportIndexModel getApplicationReportIndexModel();
 
     /**
@@ -155,4 +159,23 @@ public interface ApplicationReportModel extends ReportModel
      */
     @MapInAdjacentProperties(label = "reportProperties")
     void setReportProperties(Map<String, String> map);
+
+    abstract class Impl implements ApplicationReportModel, JavaHandlerContext<Vertex>
+    {
+        @Override
+        public ApplicationReportIndexModel getApplicationReportIndexModel()
+        {
+            ApplicationReportIndexModel result = null;
+            for (Vertex v : it().getVertices(Direction.IN, ApplicationReportIndexModel.APPLICATION_REPORT_INDEX_TO_REPORT_MODEL))
+            {
+                ApplicationReportIndexModel model = frame(v, ApplicationReportIndexModel.class);
+                if (result == null)
+                    result = model;
+                else if (!result.getProjectModels().iterator().hasNext() && model.getProjectModels().iterator().hasNext())
+                    result = model;
+            }
+            return result;
+        }
+    }
+
 }
