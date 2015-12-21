ba59fedaeaf16b71d2f5a6aa28e512c0c96dde42
Matej Briškár <mbriskar@gmail.com>
2015-12-21 13:35:38 +0100

    WINDUP-872: Multiple Applications: File Source report leaked combine links to all XSL transformations

---
+++ rules-java-ee/tests/src/test/java/org/jboss/windup/rules/apps/javaee/tests/GenerateJBossEjbDescriptorTest.java
@@ -0,0 +1,102 @@
+package org.jboss.windup.rules.apps.javaee.tests;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.forge.arquillian.AddonDependencies;
+import org.jboss.forge.arquillian.AddonDependency;
+import org.jboss.forge.arquillian.archive.AddonArchive;
+import org.jboss.forge.furnace.util.OperatingSystemUtils;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.windup.config.RuleProvider;
+import org.jboss.windup.graph.GraphContext;
+import org.jboss.windup.graph.GraphContextFactory;
+import org.jboss.windup.graph.model.LinkModel;
+import org.jboss.windup.graph.model.ProjectModel;
+import org.jboss.windup.graph.model.WindupConfigurationModel;
+import org.jboss.windup.graph.model.resource.FileModel;
+import org.jboss.windup.graph.service.GraphService;
+import org.jboss.windup.rules.apps.javaee.model.EjbDeploymentDescriptorModel;
+import org.jboss.windup.rules.apps.javaee.model.WebXmlModel;
+import org.jboss.windup.rules.apps.javaee.rules.jboss.GenerateJBossEjbDescriptorRuleProvider;
+import org.jboss.windup.rules.apps.javaee.rules.jboss.GenerateJBossWebDescriptorRuleProvider;
+import org.jboss.windup.testutil.basics.WindupTestUtilMethods;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.inject.Inject;
+import java.nio.file.Path;
+import java.util.Collections;
+
+/**
+ *  Tests the {@link GenerateJBossEjbDescriptorRuleProvider}
+ */
+@RunWith(Arquillian.class)
+public class GenerateJBossEjbDescriptorTest
+{
+    @Deployment
+    @AddonDependencies
+    public static AddonArchive getDeployment()
+    {
+        final AddonArchive archive = ShrinkWrap.create(AddonArchive.class)
+                    .addBeansXML();
+        return archive;
+    }
+
+    @Inject
+    private GraphContextFactory factory;
+
+    @Test
+    public void testRuleProviders() throws Exception
+    {
+        final Path folder = OperatingSystemUtils.createTempDir().toPath();
+        try (final GraphContext context = factory.create(folder))
+        {
+            initData(context);
+            WindupTestUtilMethods.runOnlyRuleProviders(Collections.singletonList(new GenerateJBossEjbDescriptorRuleProvider()), context);
+            checkWebXmls(context);
+        }
+    }
+
+    public void initData(GraphContext context)
+    {
+        ProjectModel pm1 =context.getFramed().addVertex(null, ProjectModel.class);
+        ProjectModel pm2 =context.getFramed().addVertex(null, ProjectModel.class);
+        FileModel fm1 =context.getFramed().addVertex(null, FileModel.class);
+        FileModel fm2 =context.getFramed().addVertex(null, FileModel.class);
+
+        fm1.setProjectModel(pm1);
+        fm2.setProjectModel(pm2);
+        WindupConfigurationModel configurationModel =context.getFramed().addVertex(null, WindupConfigurationModel.class);
+        GraphService<ProjectModel> projectModels = new GraphService<>(context, ProjectModel.class);
+        GraphService<EjbDeploymentDescriptorModel> ejbDescriptors = new GraphService<>(context, EjbDeploymentDescriptorModel.class);
+        for (ProjectModel projectModel : projectModels.findAll())
+        {
+            EjbDeploymentDescriptorModel ejbDescriptor =  ejbDescriptors.create();
+            ejbDescriptor.setProjectModel(projectModel);
+        }
+        configurationModel.addInputPath(fm1);
+        configurationModel.addInputPath(fm2);
+    }
+
+
+    private void checkWebXmls(GraphContext context)
+    {
+        GraphService<EjbDeploymentDescriptorModel> ejbDescriptors = new GraphService<>(context,EjbDeploymentDescriptorModel.class);
+        for (EjbDeploymentDescriptorModel ejbDesc : ejbDescriptors.findAll())
+        {
+            Assert.assertTrue(1 >= getIterableSize(ejbDesc.getLinksToTransformedFiles()));
+        }
+    }
+
+    private int getIterableSize(Iterable<LinkModel> linksToTransformedFiles)
+    {
+        int resultCount = 0;
+        for (LinkModel linksToTransformedFile : linksToTransformedFiles)
+        {
+            resultCount++;
+        }
+        return resultCount;
+    }
+
+}
---
+++ test-files/badly_named_app/src/main/java/org/windup/examples/ejb/entitybean/SimpleEntityNoTableName.java
@@ -0,0 +1,8 @@
+package org.windup.examples.ejb.entitybean;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+public class SimpleEntityNoTableName {
+}
