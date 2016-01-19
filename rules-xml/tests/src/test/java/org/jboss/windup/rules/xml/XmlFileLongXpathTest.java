81850446b445e32e213f22210c24a9f49090caee
Matej Briškár <mbriskar@gmail.com>
2016-01-19 13:00:25 +0100

    WINDUPRULE-109: Long Xpaths suffered from not executed parts

---
+++ rules-xml/tests/src/test/java/org/jboss/windup/rules/xml/XmlFileLongXpathTest.java
@@ -0,0 +1,131 @@
+package org.jboss.windup.rules.xml;
+
+import org.apache.commons.io.FileUtils;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.forge.arquillian.AddonDependencies;
+import org.jboss.forge.arquillian.AddonDependency;
+import org.jboss.forge.arquillian.archive.AddonArchive;
+import org.jboss.forge.furnace.util.Iterators;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.windup.config.AbstractRuleProvider;
+import org.jboss.windup.config.metadata.MetadataBuilder;
+import org.jboss.windup.config.operation.Iteration;
+import org.jboss.windup.config.phase.MigrationRulesPhase;
+import org.jboss.windup.config.phase.PostMigrationRulesPhase;
+import org.jboss.windup.config.phase.ReportGenerationPhase;
+import org.jboss.windup.exec.WindupProcessor;
+import org.jboss.windup.exec.configuration.WindupConfiguration;
+import org.jboss.windup.exec.rulefilters.NotPredicate;
+import org.jboss.windup.exec.rulefilters.RuleProviderPhasePredicate;
+import org.jboss.windup.graph.GraphContext;
+import org.jboss.windup.graph.GraphContextFactory;
+import org.jboss.windup.graph.model.ProjectModel;
+import org.jboss.windup.graph.model.resource.FileModel;
+import org.jboss.windup.graph.service.GraphService;
+import org.jboss.windup.reporting.config.Hint;
+import org.jboss.windup.reporting.config.classification.Classification;
+import org.jboss.windup.reporting.model.ClassificationModel;
+import org.jboss.windup.reporting.model.InlineHintModel;
+import org.jboss.windup.rules.apps.xml.condition.XmlFile;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ocpsoft.rewrite.config.Configuration;
+import org.ocpsoft.rewrite.config.ConfigurationBuilder;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Testing {@link XmlFile} condition with a long xpath that registers multiple startFrame()/persist functions in the complicated fashion.
+ */
+@RunWith(Arquillian.class)
+public class XmlFileLongXpathTest
+{
+
+    public static final String LONG_XPATHS_WORK_FINE_CLASSIFICATION = "Long xpaths work fine!";
+
+    @Deployment
+    @AddonDependencies({
+                @AddonDependency(name = "org.jboss.windup.config:windup-config"),
+                @AddonDependency(name = "org.jboss.windup.exec:windup-exec"),
+                @AddonDependency(name = "org.jboss.windup.rules.apps:windup-rules-java"),
+                @AddonDependency(name = "org.jboss.windup.rules.apps:windup-rules-base"),
+                @AddonDependency(name = "org.jboss.windup.rules.apps:windup-rules-xml"),
+                @AddonDependency(name = "org.jboss.windup.reporting:windup-reporting"),
+                @AddonDependency(name = "org.jboss.forge.furnace.container:cdi")
+    })
+    public static AddonArchive getDeployment()
+    {
+        return ShrinkWrap.create(AddonArchive.class).addBeansXML();
+    }
+
+    @Inject
+    private WindupProcessor processor;
+
+    @Inject
+    private GraphContextFactory factory;
+
+    @Test
+    public void test() throws IOException
+    {
+        try (GraphContext context = factory.create())
+        {
+            ProjectModel pm = context.getFramed().addVertex(null, ProjectModel.class);
+            pm.setName("Main Project");
+            FileModel inputPath = context.getFramed().addVertex(null, FileModel.class);
+            inputPath.setFilePath("src/test/resources/");
+
+            Path outputPath = Paths.get(FileUtils.getTempDirectory().toString(), "windup_"
+                        + UUID.randomUUID().toString());
+            FileUtils.deleteDirectory(outputPath.toFile());
+            Files.createDirectories(outputPath);
+
+            inputPath.setProjectModel(pm);
+            pm.setRootFileModel(inputPath);
+
+            WindupConfiguration windupConfiguration = new WindupConfiguration()
+                        .setRuleProviderFilter(new NotPredicate(
+                                    new RuleProviderPhasePredicate(MigrationRulesPhase.class, ReportGenerationPhase.class)
+                        ))
+                        .setGraphContext(context);
+            windupConfiguration.addInputPath(Paths.get(inputPath.getFilePath()));
+            windupConfiguration.setOutputDirectory(outputPath);
+            processor.execute(windupConfiguration);
+
+            GraphService<ClassificationModel> classificationService = new GraphService<>(context,
+                        ClassificationModel.class);
+            List<ClassificationModel> classifications = Iterators.asList(classificationService.findAll());
+            Assert.assertEquals(1, classifications.size());
+            Assert.assertEquals(LONG_XPATHS_WORK_FINE_CLASSIFICATION, classifications.get(0).getClassification());
+        }
+    }
+
+    @Singleton
+    public static class TestXMLNestedXmlFileRuleProvider extends AbstractRuleProvider
+    {
+        public TestXMLNestedXmlFileRuleProvider()
+        {
+            super(MetadataBuilder.forProvider(TestXMLNestedXmlFileRuleProvider.class).setPhase(PostMigrationRulesPhase.class));
+        }
+
+        // @formatter:off
+        @Override
+        public Configuration getConfiguration(GraphContext context)
+        {
+            return ConfigurationBuilder
+                        .begin()
+                        .addRule()
+                        .when(XmlFile.matchesXpath("//1:randomElement[contains(text(),'first')] | //2:randomElement[contains(text(),'first')] | //randomElement[contains(text(),'first')]").namespace("1","fake-namespace-1").namespace("2", "fake-namespace-2"))
+                        .perform(Classification.as(LONG_XPATHS_WORK_FINE_CLASSIFICATION));
+        }
+    }
+
+}
