1591ed88b1ea530f1c71742b5517e87c5183d498
Jess Sightler <jesse.sightler@gmail.com>
2016-03-28 15:38:34 -0400

    Task #57 - Make sure that all jar manifest information is available

---
+++ rules-java/api/src/main/java/org/jboss/windup/rules/apps/java/scan/provider/DiscoverAdditionalProjectDetails.java
@@ -0,0 +1,87 @@
+package org.jboss.windup.rules.apps.java.scan.provider;
+
+import org.apache.commons.lang.StringUtils;
+import org.jboss.windup.config.AbstractRuleProvider;
+import org.jboss.windup.config.GraphRewrite;
+import org.jboss.windup.config.metadata.MetadataBuilder;
+import org.jboss.windup.config.operation.iteration.AbstractIterationOperation;
+import org.jboss.windup.config.phase.DiscoverProjectStructurePhase;
+import org.jboss.windup.config.query.Query;
+import org.jboss.windup.graph.GraphContext;
+import org.jboss.windup.graph.model.ArchiveModel;
+import org.jboss.windup.graph.model.ProjectModel;
+import org.jboss.windup.rules.apps.java.model.JarManifestModel;
+import org.jboss.windup.rules.apps.java.service.JarManifestService;
+import org.ocpsoft.rewrite.config.Configuration;
+import org.ocpsoft.rewrite.config.ConfigurationBuilder;
+import org.ocpsoft.rewrite.context.EvaluationContext;
+
+/**
+ * @author <a href="mailto:jesse.sightler@gmail.com">Jesse Sightler</a>
+ */
+public class DiscoverAdditionalProjectDetails extends AbstractRuleProvider
+{
+    public DiscoverAdditionalProjectDetails()
+    {
+        super(MetadataBuilder.forProvider(DiscoverAdditionalProjectDetails.class)
+                    .setPhase(DiscoverProjectStructurePhase.class)
+                    .addExecuteAfter(DiscoverNonMavenArchiveProjectsRuleProvider.class));
+    }
+
+    @Override
+    public Configuration getConfiguration(GraphContext context)
+    {
+        return ConfigurationBuilder.begin()
+                    .addRule()
+                    .when(Query.fromType(ArchiveModel.class))
+                    .perform(new AbstractIterationOperation<ArchiveModel>()
+                    {
+                        @Override
+                        public void perform(GraphRewrite event, EvaluationContext context, ArchiveModel payload)
+                        {
+                            getAdditionalProjectDetails(event.getGraphContext(), payload);
+                        }
+                    });
+    }
+
+    private void getAdditionalProjectDetails(GraphContext context, ArchiveModel archiveModel)
+    {
+        ProjectModel projectModel = archiveModel.getProjectModel();
+
+        /*
+         * This likely means that we were ignoring the project or did not analyze it for some other reason.
+         *
+         * Just skip it in this case
+         */
+        if (projectModel == null)
+            return;
+
+        JarManifestService service = new JarManifestService(context);
+        Iterable<JarManifestModel> manifests = service.getManifestsByArchive(archiveModel);
+
+        // get any properties from the manifest files that we don't already have from other sources (eg, maven pom)
+        for (JarManifestModel manifest : manifests)
+        {
+            if (StringUtils.isNotBlank(manifest.getName())
+                        && (projectModel.getName() == null || projectModel.getName().equals(archiveModel.getArchiveName())))
+            {
+                projectModel.setName(manifest.getName());
+            }
+
+            if (StringUtils.isNotBlank(manifest.getVendor()) && StringUtils.isBlank(projectModel.getOrganization()))
+            {
+                projectModel.setOrganization(manifest.getVendor());
+            }
+
+            if (StringUtils.isNotBlank(manifest.getVersion()) && StringUtils.isBlank(projectModel.getVersion()))
+            {
+                projectModel.setVersion(manifest.getVersion());
+            }
+
+            if (StringUtils.isNotBlank(manifest.getDescription()) && StringUtils.isBlank(projectModel.getDescription()))
+            {
+                projectModel.setDescription(manifest.getDescription());
+            }
+        }
+    }
+}
--- rules-java/api/src/main/java/org/jboss/windup/rules/apps/java/service/JarManifestService.java
+++ rules-java/api/src/main/java/org/jboss/windup/rules/apps/java/service/JarManifestService.java
@@ -1,6 +1,10 @@
 package org.jboss.windup.rules.apps.java.service;
 
+import com.tinkerpop.blueprints.Vertex;
+import com.tinkerpop.frames.structures.FramedVertexIterable;
+import com.tinkerpop.gremlin.java.GremlinPipeline;
 import org.jboss.windup.graph.GraphContext;
+import org.jboss.windup.graph.model.ArchiveModel;
 import org.jboss.windup.graph.service.GraphService;
 import org.jboss.windup.rules.apps.java.model.JarManifestModel;
 
@@ -15,5 +19,15 @@ public class JarManifestService extends GraphService<JarManifestModel>
     public JarManifestService(GraphContext context)
     {
         super(context, JarManifestModel.class);
+    }
+
+    /**
+     * Gets all {@link JarManifestModel}s associated with this archive.
+     */
+    public Iterable<JarManifestModel> getManifestsByArchive(ArchiveModel archiveModel)
+    {
+        GremlinPipeline<Vertex, Vertex> pipeline = new GremlinPipeline<>(archiveModel.asVertex());
+        pipeline.out(JarManifestModel.ARCHIVE);
+        return new FramedVertexIterable<>(getGraphContext().getFramed(), pipeline, JarManifestModel.class);
     }
 }
