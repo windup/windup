8661959dcdf46c02902fb0e6c522a7cf7ac066bb
Jess Sightler <jesse.sightler@gmail.com>
2016-05-04 10:11:49 -0400

    Updated to a more automated release script

--- build/release_windup.sh
+++ build/release_windup.sh
@@ -7,15 +7,40 @@ then
    . $HOME/.windup_profile
 fi
 
+REL=$1
+DEV=$2
+
 function release_windup {
-        REL=$1
-        DEV=$2
-        REPO=$3
-        REPODIR=$4
-#        git clone $REPO $REPODIR
+        REPO=$1
+        REPODIR=$2
+
         cd $REPODIR
-        echo Releasing \"$REPO\" - $1 \(Next dev version is $2\)
-        mvn release:prepare release:perform \
+        echo Releasing \"$REPO\" - $REL \(Next dev version is $DEV\)
+        mvn release:prepare-with-pom clean install \
+                -DdevelopmentVersion=$DEV \
+                -DreleaseVersion=$REL \
+                -Dtag=$REL \
+                -DskipTests \
+                -Darguments=-DskipTests \
+                -Dgwt.compiler.skip=true \
+                -Dmvn.test.skip=true \
+                -Dfurnace.dot.skip
+
+        mvn -DskipTests clean install
+
+        mvn release:prepare clean install \
+                -DdevelopmentVersion=$DEV \
+                -DreleaseVersion=$REL \
+                -Dtag=$REL \
+                -DskipTests \
+                -Darguments=-DskipTests \
+                -Dgwt.compiler.skip=true \
+                -Dmvn.test.skip=true \
+                -Dfurnace.dot.skip
+
+        echo "Finished preparing release"
+
+        mvn release:perform \
                 -DdevelopmentVersion=$DEV \
                 -DreleaseVersion=$REL \
                 -Dtag=$REL \
@@ -30,16 +55,44 @@ function release_windup {
 WORK_DIR="windup_tmp_dir"
 echo "Working in temp directory $WORK_DIR"
 echo "Cleaning any previous contents from $WORK_DIR"
-#rm -rf $WORK_DIR
-#mkdir $WORK_DIR
+rm -rf $WORK_DIR
+mkdir $WORK_DIR
 cd $WORK_DIR
-#git clone git@github.com:windup/windup.git
-#git clone git@github.com:windup/windup-rulesets.git
-#git clone git@github.com:windup/windup-distribution.git
+git clone git@github.com:windup/windup.git
+git clone git@github.com:windup/windup-rulesets.git
+git clone git@github.com:windup/windup-distribution.git
 
-#release_windup $1 $2 git@github.com:windup/windup.git windup
-#release_windup $1 $2 git@github.com:windup/windup-rulesets.git windup-rulesets
-release_windup $1 $2 git@github.com:windup/windup-distribution.git windup-distribution
+cd windup-rulesets
+sed -i -e "s/<version.windup>.*<\/version.windup>/<version.windup>$REL<\/version.windup>/g" pom.xml
+git add -A
+git commit -a -m "Preparing for release"
+git push origin
+cd ../
+
+cd windup-distribution
+sed -i -e "s/<version.windup>.*<\/version.windup>/<version.windup>$REL<\/version.windup>/g" pom.xml
+git add -A
+git commit -a -m "Preparing for release"
+git push origin
+cd ../
+
+release_windup git@github.com:windup/windup.git windup
+release_windup git@github.com:windup/windup-rulesets.git windup-rulesets
+release_windup git@github.com:windup/windup-distribution.git windup-distribution
+
+cd windup-rulesets
+sed -i -e "s/<version.windup>.*<\/version.windup>/<version.windup>$DEV<\/version.windup>/g" pom.xml
+git add -A
+git commit -a -m "Back to development"
+git push origin
+cd ../
+
+cd windup-distribution
+sed -i -e "s/<version.windup>.*<\/version.windup>/<version.windup>$DEV<\/version.windup>/g" pom.xml
+git add -A
+git commit -a -m "Back to development"
+git push origin
+cd ../
 
 #open https://repository.jboss.org/nexus/index.html
 #echo "Cleaning up temp directory $WORK_DIR"
