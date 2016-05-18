a167b21bc22b1ff174ce88b7ccd594f84938916a
Jess Sightler <jesse.sightler@gmail.com>
2016-05-18 11:59:55 -0400

    WINDUP-997: spring-mvc.xml as invalid by 'XML File is not valid' rule

--- rules-xml/impl/src/main/java/org/jboss/windup/rules/apps/xml/xml/ValidateXmlFilesRuleProvider.java
+++ rules-xml/impl/src/main/java/org/jboss/windup/rules/apps/xml/xml/ValidateXmlFilesRuleProvider.java
@@ -1,16 +1,33 @@
 package org.jboss.windup.rules.apps.xml.xml;
 
+import java.io.FileNotFoundException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.UnknownHostException;
+import java.util.Collections;
+
+import javax.xml.XMLConstants;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+
+import org.apache.commons.lang3.StringUtils;
 import org.jboss.windup.config.AbstractRuleProvider;
 import org.jboss.windup.config.GraphRewrite;
+import org.jboss.windup.config.condition.EvaluationStrategy;
 import org.jboss.windup.config.metadata.MetadataBuilder;
 import org.jboss.windup.config.operation.iteration.AbstractIterationOperation;
 import org.jboss.windup.config.phase.MigrationRulesPhase;
 import org.jboss.windup.config.query.Query;
 import org.jboss.windup.graph.GraphContext;
 import org.jboss.windup.graph.model.resource.FileModel;
+import org.jboss.windup.graph.model.resource.SourceFileModel;
 import org.jboss.windup.graph.service.GraphService;
 import org.jboss.windup.reporting.model.ClassificationModel;
 import org.jboss.windup.reporting.model.InlineHintModel;
+import org.jboss.windup.reporting.model.Severity;
 import org.jboss.windup.reporting.service.ClassificationService;
 import org.jboss.windup.reporting.service.InlineHintService;
 import org.jboss.windup.reporting.service.TagSetService;
@@ -22,22 +39,12 @@ import org.jboss.windup.util.xml.LocationAwareContentHandler;
 import org.jboss.windup.util.xml.XmlUtil;
 import org.ocpsoft.rewrite.config.Configuration;
 import org.ocpsoft.rewrite.config.ConfigurationBuilder;
+import org.ocpsoft.rewrite.config.Rule;
 import org.ocpsoft.rewrite.context.EvaluationContext;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
-
-import javax.xml.XMLConstants;
-import javax.xml.transform.Source;
-import javax.xml.transform.stream.StreamSource;
-import javax.xml.validation.Schema;
-import javax.xml.validation.SchemaFactory;
-import javax.xml.validation.Validator;
-import java.io.FileNotFoundException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.UnknownHostException;
-import java.util.Collections;
+import org.xml.sax.SAXParseException;
 
 /**
  * A rule provider validating all of the xml files and registering the classification in case the xml file is not valid.
@@ -64,7 +71,7 @@ public class ValidateXmlFilesRuleProvider extends AbstractRuleProvider
         super(MetadataBuilder.forProvider(ValidateXmlFilesRuleProvider.class)
                     .setPhase(MigrationRulesPhase.class)
                     .setHaltOnException(true));
-        this.schemaFactory=schemaFactory;
+        this.schemaFactory = schemaFactory;
     }
 
     @Override
@@ -84,45 +91,49 @@ public class ValidateXmlFilesRuleProvider extends AbstractRuleProvider
             XmlFileService xmlFileService = new XmlFileService(event.getGraphContext());
             Document document = xmlFileService.loadDocumentQuiet(context, sourceFile);
             String xsdUrl = getXSDSchemaLocationUrl(document);
+
+            if (StringUtils.isBlank(xsdUrl))
+                return;
+
             try
             {
-                if(xsdUrl != null && !xsdUrl.isEmpty() && !isXmlValid(sourceFile,xsdUrl)) {
-                    ClassificationService classificationService = new ClassificationService(event.getGraphContext());
-                    ClassificationModel model =classificationService.attachClassification(context, sourceFile, XmlFileModel.NOT_VALID_XML,
-                                null);
-                    TagSetService tagSetService = new TagSetService(event.getGraphContext());
-                    model.setTagModel(tagSetService.getOrCreate(event, Collections.singleton(NOT_VALID_XML_TAG)));
-                }
+                validateXml(event, context, sourceFile, xsdUrl);
             }
             catch (MalformedURLException e)
             {
-                FileLocationModel rootElementLocation = createLocationModelFromNodeElement(document.getDocumentElement(),event.getGraphContext(),sourceFile);
+                FileLocationModel rootElementLocation = createLocationModelFromNodeElement(document.getDocumentElement(), event.getGraphContext(),
+                            sourceFile);
                 createUrlNotValidHint(event.getGraphContext(), rootElementLocation, xsdUrl);
             }
             catch (Exception e)
             {
                 final Throwable cause = e.getCause();
-                if(cause instanceof UnknownHostException) {
-                    //probably no internet connection. Do not do anything
-                } else if(cause instanceof FileNotFoundException) {
-                    //probably wrong XSD URL
-                    FileLocationModel rootElementLocation = createLocationModelFromNodeElement(document.getDocumentElement(),event.getGraphContext(),sourceFile);
-                    createUrlNotValidHint(event.getGraphContext(),rootElementLocation,xsdUrl);
-
+                if (cause instanceof UnknownHostException)
+                {
+                    // probably no internet connection. Do not do anything
+                }
+                else if (cause instanceof FileNotFoundException)
+                {
+                    // probably wrong XSD URL
+                    FileLocationModel rootElementLocation = createLocationModelFromNodeElement(document.getDocumentElement(), event.getGraphContext(),
+                                sourceFile);
+                    createUrlNotValidHint(event.getGraphContext(), rootElementLocation, xsdUrl);
 
                 }
             }
 
         }
 
-        private void createUrlNotValidHint(GraphContext context,FileLocationModel model, String xsdUrl) {
+        private void createUrlNotValidHint(GraphContext context, FileLocationModel model, String xsdUrl)
+        {
             final InlineHintModel inlineHintModel = InlineHintService.addTypeToModel(context, model, InlineHintModel.class);
             inlineHintModel.setTitle(XmlFileModel.XSD_URL_NOT_VALID);
             inlineHintModel.setHint(xsdUrl + " is not a valid url.");
             inlineHintModel.setEffort(1);
         }
 
-        private FileLocationModel createLocationModelFromNodeElement(Node node, GraphContext context, FileModel sourceFile) {
+        private FileLocationModel createLocationModelFromNodeElement(Node node, GraphContext context, FileModel sourceFile)
+        {
             int lineNumber = (int) node.getUserData(
                         LocationAwareContentHandler.LINE_NUMBER_KEY_NAME);
             int columnNumber = (int) node.getUserData(
@@ -141,35 +152,72 @@ public class ValidateXmlFilesRuleProvider extends AbstractRuleProvider
             return fileLocation;
         }
 
-        private String getXSDSchemaLocationUrl(Document document) {
-            if(document == null) {
+        private String getXSDSchemaLocationUrl(Document document)
+        {
+            if (document == null)
+            {
                 return null;
             }
 
-            String xsdSchemaAttr =document.getDocumentElement().getAttributeNS(XMLSCHEMA_URL, XMLSCHEMA_ATTRIBUTE_NAME);
-            if(xsdSchemaAttr == null) {
+            String xsdSchemaAttr = document.getDocumentElement().getAttributeNS(XMLSCHEMA_URL, XMLSCHEMA_ATTRIBUTE_NAME);
+            if (xsdSchemaAttr == null)
+            {
                 return null;
             }
             String[] splittedXslSchemaAttr = xsdSchemaAttr.split(" ");
-            return splittedXslSchemaAttr[splittedXslSchemaAttr.length-1];
+            return splittedXslSchemaAttr[splittedXslSchemaAttr.length - 1];
         }
 
-        private boolean isXmlValid(XmlFileModel sourceFile, String xsdUrl) throws MalformedURLException, SAXException
+        private void validateXml(GraphRewrite event, EvaluationContext context, XmlFileModel sourceFile, String xsdUrl) throws MalformedURLException, SAXException
         {
             URL schemaFile = new URL(xsdUrl);
             Source xmlFile = new StreamSource(sourceFile.asFile());
             Schema schema = schemaFactory.newSchema(schemaFile);
             Validator validator = schema.newValidator();
-            try {
+            boolean parseFailed = true;
+            try
+            {
                 validator.validate(xmlFile);
-            } catch (SAXException e) {
-                return false;
+                parseFailed = false;
+            }
+            catch (SAXParseException e)
+            {
+                int lineNumber = e.getLineNumber();
+                int column = e.getColumnNumber();
+
+                InlineHintService service = new InlineHintService(event.getGraphContext());
+                InlineHintModel hintModel = service.create();
+                hintModel.setRuleID(((Rule) context.get(Rule.class)).getId());
+                hintModel.setLineNumber(lineNumber);
+                hintModel.setColumnNumber(column);
+
+                // FIXME - Fake value as we don't get an actual length of the error from the parser
+                hintModel.setLength(1);
+                hintModel.setFile(sourceFile);
+                hintModel.setEffort(0);
+                hintModel.setSeverity(Severity.POTENTIAL);
+                hintModel.setTitle(XmlFileModel.NOT_VALID_XML);
+                String message = "XSD Validation failed due to:\n\n";
+                message += "\t" + e.getMessage();
+                message += "\n\n";
+
+                hintModel.setHint(message);
             }
             catch (Exception e)
             {
-                return false;
+                parseFailed = true;
             }
-            return true;
+
+            if (parseFailed)
+            {
+                sourceFile.setGenerateSourceReport(true);
+
+                ClassificationService classificationService = new ClassificationService(event.getGraphContext());
+                ClassificationModel model = classificationService.attachClassification(context, sourceFile, XmlFileModel.NOT_VALID_XML,
+                        null);
+                TagSetService tagSetService = new TagSetService(event.getGraphContext());
+                model.setTagModel(tagSetService.getOrCreate(event, Collections.singleton(NOT_VALID_XML_TAG)));
+            }
         }
     }
 }
