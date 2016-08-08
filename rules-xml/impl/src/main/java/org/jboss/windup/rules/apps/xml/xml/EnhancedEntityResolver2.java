086c8434776b49f57931165c76ffba7907295c08
Marek Novotný <hotmana76@gmail.com>
2016-08-08 13:04:40 +0200

    WINDUP-1099 adding resolving W3c dtd/xsd definitions locally and not remotely

--- rules-xml/impl/src/main/java/org/jboss/windup/rules/apps/xml/xml/EnhancedEntityResolver2.java
+++ rules-xml/impl/src/main/java/org/jboss/windup/rules/apps/xml/xml/EnhancedEntityResolver2.java
@@ -4,18 +4,34 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.logging.Logger;
 
+import org.apache.xml.resolver.tools.CatalogResolver;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.xml.sax.ext.EntityResolver2;
 
 /**
  * This is an {@link EntityResolver2} that has been enhanced to support URL redirection.
+ * 
+ * It also use local stored DTDs/XSDs as possible not to slow down validation.
+ * see https://www.w3.org/blog/systeam/2008/02/08/w3c_s_excessive_dtd_traffic/ 
  *
  * @author <a href="mailto:jesse.sightler@gmail.com">Jesse Sightler</a>
+ * @author <a href="mailto:hotmana76@gmail.com">Marek Novotny</a>
  */
 public class EnhancedEntityResolver2 implements EntityResolver2
 {
+    private static final Logger LOG = Logger.getLogger(EnhancedEntityResolver2.class.getSimpleName());
+
+    /* this is catalog resolver definition for resolving entities/schemas locally instead of remotely
+     * default catalogs settings are in CatalogManager.properties file available on classpath,
+     * There is a default settings in windup/rules-xml-api module as rules-xml/api/src/main/resources/CatalogManager.properties
+     * and there is also default catalog with HTML/XHTML/XML local definitions.
+     * 
+     */
+    private final static CatalogResolver catalogResolver = new CatalogResolver();
+    
     @Override
     public InputSource getExternalSubset(String name, String baseURI) throws SAXException, IOException
     {
@@ -31,8 +47,17 @@ public class EnhancedEntityResolver2 implements EntityResolver2
     @Override
     public InputSource resolveEntity(String name, String publicId, String baseURI, String systemId) throws SAXException, IOException
     {
+        LOG.fine("Entity for resolving " + publicId + " " + systemId);
+        // try to resolve first from catalog if it doesn't find entity continue with other ways 
+        InputSource inputSource = catalogResolver.resolveEntity(publicId, systemId);        
+        if (inputSource != null)
+        {
+            LOG.fine("Resolved entity through catalog.");
+            return inputSource;
+        }
+        
         URL url = baseURI != null ? new URL(new URL(baseURI), systemId) : new URL(systemId);
-
+        LOG.fine("Resolving entity -> " + url.toString());
         URLConnection connection = url.openConnection();
 
         // Not a HTTP connection... skip redirection logic
@@ -49,7 +74,7 @@ public class EnhancedEntityResolver2 implements EntityResolver2
             status = httpConnection.getResponseCode();
         }
 
-        InputSource inputSource = new InputSource(httpConnection.getInputStream());
+        inputSource = new InputSource(httpConnection.getInputStream());
         inputSource.setSystemId(url.toString());
         inputSource.setPublicId(publicId);
         return inputSource;
@@ -63,4 +88,5 @@ public class EnhancedEntityResolver2 implements EntityResolver2
                         || status == HttpURLConnection.HTTP_MOVED_PERM
                         || status == HttpURLConnection.HTTP_SEE_OTHER);
     }
+
 }
