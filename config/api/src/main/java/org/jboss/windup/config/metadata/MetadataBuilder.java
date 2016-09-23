bf49f24f1fc152e0c85ed7bb632728f1ed2d1f5e
Ondrej Zizka <zizka@seznam.cz>
2016-09-23 15:38:20 +0200

    Mute excessive logging - Is it disabled? (#1016)

--- config/api/src/main/java/org/jboss/windup/config/metadata/MetadataBuilder.java
+++ config/api/src/main/java/org/jboss/windup/config/metadata/MetadataBuilder.java
@@ -147,7 +147,8 @@ public class MetadataBuilder extends AbstractRulesetMetadata implements RuleProv
 
         builder.haltOnException = metadata.haltOnException();
         builder.disabled = metadata.disabled();
-        LOG.info("Is it disabled? " + providerId + " " + builder.disabled);///
+        if (builder.disabled)
+            LOG.info("Disabled provider: " + providerId);
 
         return builder;
     }
