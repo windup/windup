bbd799564c0618f379b4fc0d0cc06776c31ee3db
Ondrej Zizka <zizka@seznam.cz>
2016-10-18 04:10:35 +0200

    Tech report WIP

---
+++ rules-java-ee/addon/src/main/java/org/jboss/windup/rules/apps/javaee/model/stats/GeneralStatsItemModel.java
@@ -0,0 +1,26 @@
+package org.jboss.windup.rules.apps.javaee.model.stats;
+
+import com.tinkerpop.frames.Property;
+import com.tinkerpop.frames.modules.typedgraph.TypeValue;
+import org.jboss.windup.graph.model.WindupVertexFrame;
+
+/**
+ * A single stats item.
+ */
+@TypeValue(GeneralStatsItemModel.TYPE)
+public interface GeneralStatsItemModel extends WindupVertexFrame
+{
+    String TYPE = "GeneralStatsItem";
+    String KEY      = TYPE + "_key";
+    String LABEL    = TYPE + "_label";
+    String QUANTITY = TYPE + "_qty";
+
+    @Property(KEY) String getKey();
+    @Property(KEY) GeneralStatsItemModel setKey(String key);
+    
+    @Property(LABEL) String getLabel();
+    @Property(LABEL) GeneralStatsItemModel getLabel(String label);
+    
+    @Property(QUANTITY) int getQuantity();
+    @Property(QUANTITY) GeneralStatsItemModel setQuantity(int qty);
+}
---
+++ rules-java-ee/addon/src/main/java/org/jboss/windup/rules/apps/javaee/model/stats/generateModel.groovy
@@ -0,0 +1,68 @@
+package org.jboss.windup.reporting.stats;
+
+import java.lang.String;
+
+/**
+ * Generates the source snippets for statistics: 1) Frames model 2) Angular template.
+ * 
+ * @author <a href="mailto:zizka@seznam.cz">Ondrej Zizka</a>
+ */
+
+def items2 = [
+    [key: "stats.files.byType.java.percent", label: "Java"],
+    [key: "stats.files.byType.html.percent", label: "HTML"],
+    [key: "stats.files.byType.xml.percent",  label: "XML"],
+    [key: "stats.files.byType.fmt.percent",  label: "FreeMarker"],
+    [key: "stats.files.byType.js.percent",   label: "JavaScript"],
+    [key: "stats.files.byType.css.percent",  label: "CSS"],
+
+    [key: "stats.services.ejb.stateless", label: "stateless"],
+    [key: "stats.services.ejb.stateful", label: "stateful"],
+    [key: "stats.services.ejb.messageDriven", label: "message driven"],
+    [key: "stats.services.http.jax-rs", label: "JAX-RS services"],
+    [key: "stats.services.http.jax-ws", label: "JAX-WS services"],
+    [key: "stats.services.jpa.persistenceUnits", label: "persistence units"],
+    [key: "stats.services.jpa.namedQueries", label: "JPA named queries"],
+    [key: "stats.services.jpa.entitites", label: "JPA entities"],
+    [key: "stats.services.rmi.services", label: "RMI services"],
+    
+    [key: "stats.serverResources.db.jdbcDatasources", label: "JDBC datasources"],
+    [key: "stats.serverResources.db.xaJdbcDatasources", label: "XA JDBC datasources"],
+    [key: "stats.serverResources.msg.jms.queues", label: "JMS queues"],
+    [key: "stats.serverResources.msg.jms.topics", label: "JMS topics"],
+    [key: "stats.serverResources.msg.jms.connectionFactories", label: "connection factories"],
+    [key: "stats.serverResources.security.realms", label: "security realms"],
+    [key: "stats.serverResources.jndi.totalEntries", label: "total JNDI entries"],
+    
+    [key: "stats.java.classes.original", label: "own Java classes"],
+    [key: "stats.java.jars.original", label: "own Java JARs"],
+    [key: "stats.java.classes.total", label: "total Java classes"],
+    [key: "stats.java.jars.total", label: "total Java JARs"],
+];
+
+def ADJ = false;
+
+items2.each({item -> 
+    def key = item["key"];
+    String keyConst  = key.replaceAll(/[-\.]/, "_").toUpperCase();
+    String keyMethod = key.replaceAll(/([-\.]\w)/, {x -> x[0][1].toUpperCase()}).capitalize();
+    println "\n    String ${keyConst} = \"${key}\";"
+    if (ADJ) {
+        println "    @Adjacency(label = ${keyConst}, direction = Direction.OUT) GeneralStatsItemModel get${keyMethod}();"
+        println "    @Adjacency(label = ${keyConst}, direction = Direction.OUT) void set${keyMethod}(GeneralStatsItemModel item);";
+    } else {
+        println "    @Property(${keyConst}) int get${keyMethod}();"
+        println "    @Property(${keyConst}) TechnologiesStatsModel set${keyMethod}(int qty);";
+    }
+});
+
+println();
+
+items2.each({item -> 
+    def key = item["key"];
+    String keyConst  = key.replaceAll(/[-\.]/, "_").toUpperCase();
+    String keyMethod = key.replaceAll(/([-\.]\w)/, {x -> x[0][1].toUpperCase()}).capitalize();
+    println "<tr> <td>${item["label"]}</td> <td>{{variousStatsMap.get('${key}').quantity}}</td> </tr>";
+});
+/*
+*/
