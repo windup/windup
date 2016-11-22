7bfd8e56008cb24c4badda87032a8e78588e9735
Ondrej Zizka <zizka@seznam.cz>
2016-11-22 22:30:48 +0100

    WINDUP-1239 NPE in DiscoverDuplicateArchiveProjectsRuleProvider (#1040)
    * WINDUP-1239 NPE in DiscoverDuplicateArchiveProjectsRuleProvider
    
    * Update DiscoverDuplicateArchiveProjectsRuleProvider.java
    
    * Update DiscoverDuplicateArchiveProjectsRuleProvider.java

--- rules-java/api/src/main/java/org/jboss/windup/rules/apps/java/scan/provider/DiscoverDuplicateArchiveProjectsRuleProvider.java
+++ rules-java/api/src/main/java/org/jboss/windup/rules/apps/java/scan/provider/DiscoverDuplicateArchiveProjectsRuleProvider.java
@@ -21,6 +21,18 @@ import org.ocpsoft.rewrite.context.EvaluationContext;
  * This creates {@link DuplicateProjectModel}s, associates them with the {@link DuplicateArchiveModel}s
  * and attaches them to the canonical {@link ProjectModel}s.
  *
+ * The links between the duplicated projects and archives follow this schema:
+ * <pre>
+ *    ArchiveX.jar          <->  canonical archive  <-> ArchiveXDuplicate.jar
+ *         |                              |                      |
+ *    getProjectModel()            getProjectModel()       getProjectModel()
+ *         |                              |                      |
+ *         V                              V                      V
+ *   ArchiveX.jar's project <->  canonical project  <-> ArchiveXDuplicate.jar's project
+ * </pre>
+ * Canonical (virtual) project and archive has 1:N relation to duplicated project and archives.
+ *
+ *
  * @author <a href="mailto:jesse.sightler@gmail.com">Jesse Sightler</a>
  */
 @RuleMetadata(phase = DiscoverProjectStructurePhase.class, after = DiscoverMavenHierarchyRuleProvider.class)
@@ -52,7 +64,8 @@ public class DiscoverDuplicateArchiveProjectsRuleProvider extends AbstractRulePr
         DuplicateProjectModel duplicateProject = duplicateProjectService.create();
         duplicateProject.setCanonicalProject(canonicalProject);
         duplicateProject.setName(canonicalProject.getName());
-        duplicateProject.setParentProject(duplicateArchive.getParentArchive().getProjectModel());
+        if (duplicateArchive.getParentArchive() != null)
+            duplicateProject.setParentProject(duplicateArchive.getParentArchive().getProjectModel());
         duplicateProject.setRootFileModel(duplicateArchive);
 
         if (canonicalProject.getParentProject() == null)
