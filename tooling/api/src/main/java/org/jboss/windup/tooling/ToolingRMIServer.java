1ec87b9db6192133dcc443a417bf8218d40ec1fe
John Steele <programjsteele@gmail.com>
2017-01-10 10:48:48 -0800

    WINDUP-1274 - Removed port number. Turns out to be redundant since you need the port number in order to get the Windup ExecutionBuilder in the first place. (#1061)

--- tooling/api/src/main/java/org/jboss/windup/tooling/ToolingRMIServer.java
+++ tooling/api/src/main/java/org/jboss/windup/tooling/ToolingRMIServer.java
@@ -25,7 +25,6 @@ public class ToolingRMIServer
         try
         {
         	executionBuilder.setVersion(version);
-        	executionBuilder.setPort(port);
             Registry registry = LocateRegistry.getRegistry(port);
             try
             {
