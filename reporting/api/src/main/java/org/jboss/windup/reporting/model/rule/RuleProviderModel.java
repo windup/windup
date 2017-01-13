1b131e323a774fe7b787366ddeae1c7303b03d34
David Klingenberg <dklingenberg@gmail.com>
2017-01-13 06:04:53 +0100

    446 - rule description in graph (#1062)
    * Call ExecutionStatistics.get().end after executing method.
    Before it was calling begin twice.
    
    * Store rule related information into graph.

---
+++ reporting/api/src/main/java/org/jboss/windup/reporting/model/rule/RuleProviderModel.java
@@ -0,0 +1,33 @@
+package org.jboss.windup.reporting.model.rule;
+
+import com.tinkerpop.blueprints.Direction;
+import com.tinkerpop.frames.Adjacency;
+import com.tinkerpop.frames.Property;
+import com.tinkerpop.frames.modules.typedgraph.TypeValue;
+import org.jboss.windup.graph.model.WindupVertexFrame;
+
+/**
+ * Represents a rule provider
+ *
+ * @author <a href="mailto:dklingenberg@gmail.com">David Klingenberg</a>
+ */
+@TypeValue(RuleProviderModel.TYPE)
+public interface RuleProviderModel extends WindupVertexFrame
+{
+    String TYPE = "RuleProvider";
+
+    String RULE_PROVIDER_ID = "ruleProviderID";
+    String EXECUTED_RULES = "rules";
+
+    @Property(RULE_PROVIDER_ID)
+    String getRuleProviderID();
+
+    @Property(RULE_PROVIDER_ID)
+    RuleProviderModel setRuleProviderID(String id);
+
+    @Adjacency(label = EXECUTED_RULES, direction = Direction.OUT)
+    Iterable<RuleExecutionModel> getRules();
+
+    @Adjacency(label = EXECUTED_RULES, direction = Direction.OUT)
+    RuleProviderModel addRule(RuleExecutionModel rule);
+}
