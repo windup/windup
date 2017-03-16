c25f7dd2252d95af6d39a5623392f3daaa58e780
Ondrej Zizka <zizka@seznam.cz>
2017-03-17 00:08:46 +0100

    T550 Skip HTML reports rendering (#1078)
    * Refactor PhaseHandler, spin off RulePhaseFinder
    
    * Format
    
    * Refactor PhaseHandler, spin off RulePhaseFinder

--- config-xml/addon/src/main/java/org/jboss/windup/config/parser/xml/PhaseHandler.java
+++ config-xml/addon/src/main/java/org/jboss/windup/config/parser/xml/PhaseHandler.java
@@ -1,63 +1,39 @@
 package org.jboss.windup.config.parser.xml;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.stream.Collectors;
 
-import org.jboss.forge.furnace.Furnace;
-import org.jboss.forge.furnace.proxy.Proxies;
 import org.jboss.windup.config.exception.ConfigurationException;
-import org.jboss.windup.config.furnace.FurnaceHolder;
 import org.jboss.windup.config.parser.ElementHandler;
 import org.jboss.windup.config.parser.NamespaceElementHandler;
 import org.jboss.windup.config.parser.ParserContext;
 import org.jboss.windup.config.phase.RulePhase;
+import org.jboss.windup.config.phase.RulePhaseFinder;
 import org.w3c.dom.Element;
 
 /**
- * Sets the phase for the current ruleset
- * 
+ * Sets the phase for the current ruleset.
+ *
  * @author <a href="mailto:jesse.sightler@gmail.com">Jesse Sightler</a>
  */
 @NamespaceElementHandler(elementName = "phase", namespace = RuleProviderHandler.WINDUP_RULE_NAMESPACE)
 public class PhaseHandler implements ElementHandler<Void>
 {
 
-    private Map<String, Class<? extends RulePhase>> cachedPhases;
+    private RulePhaseFinder phaseFinder = new RulePhaseFinder();
 
     @Override
     public Void processElement(ParserContext context, Element element) throws ConfigurationException
     {
         String phaseStr = element.getTextContent().trim();
 
-        Class<? extends RulePhase> phase = getPhases().get(classNameToKey(phaseStr));
+        Class<? extends RulePhase> phase = phaseFinder.findPhase(phaseStr);
         if (phase == null)
         {
-            throw new IllegalArgumentException("Unrecognized phase \"" + phaseStr + "\"");
+            String phasesListing = phaseFinder.getAvailablePhases().stream().map(clazz -> clazz.getSimpleName()).collect(Collectors.joining("\n    "));
+            throw new IllegalArgumentException("Unrecognized phase \"" + phaseStr + "\". Available phases: \n    " + phasesListing);
         }
         context.getBuilder().setPhase(phase);
         return null;
     }
 
-    private String classNameToKey(String className)
-    {
-        return className.toUpperCase();
-    }
-
-    private Map<String, Class<? extends RulePhase>> getPhases()
-    {
-        if (cachedPhases == null)
-        {
-            cachedPhases = new HashMap<>();
-            Furnace furnace = FurnaceHolder.getFurnace();
-            for (RulePhase phase : furnace.getAddonRegistry().getServices(RulePhase.class))
-            {
-                @SuppressWarnings("unchecked")
-                Class<? extends RulePhase> unwrappedClass = (Class<? extends RulePhase>) Proxies.unwrap(phase).getClass();
-                String simpleName = unwrappedClass.getSimpleName();
-                cachedPhases.put(classNameToKey(simpleName), unwrappedClass);
-            }
-        }
-        return Collections.unmodifiableMap(cachedPhases);
-    }
 }
