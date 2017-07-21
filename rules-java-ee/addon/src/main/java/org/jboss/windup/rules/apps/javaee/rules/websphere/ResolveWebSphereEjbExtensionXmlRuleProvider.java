6d0b2ca48f43c53caa71e7367064dfb9b1b9d21b
Marek Novotný <hotmana76@gmail.com>
2017-07-21 18:00:32 +0200

    subtasks for https://issues.jboss.org/browse/WINDUPRULE-265 (#1147)
    * WINDUPRULE-267: ResolveWebSphereEjbExtensionXmlRuleProvider changed classificat title and description with new links
    
    * WINDUPRULE-268: ResolveWebSphereEjbBindingXmlRuleProvider changed classification title

--- rules-java-ee/addon/src/main/java/org/jboss/windup/rules/apps/javaee/rules/websphere/ResolveWebSphereEjbExtensionXmlRuleProvider.java
+++ rules-java-ee/addon/src/main/java/org/jboss/windup/rules/apps/javaee/rules/websphere/ResolveWebSphereEjbExtensionXmlRuleProvider.java
@@ -6,8 +6,12 @@ import org.jboss.windup.config.metadata.RuleMetadata;
 import org.jboss.windup.config.phase.InitialAnalysisPhase;
 import org.jboss.windup.config.query.Query;
 import org.jboss.windup.config.ruleprovider.IteratingRuleProvider;
+import org.jboss.windup.graph.GraphContext;
+import org.jboss.windup.graph.model.LinkModel;
 import org.jboss.windup.graph.model.resource.FileModel;
+import org.jboss.windup.graph.service.LinkService;
 import org.jboss.windup.reporting.category.IssueCategoryRegistry;
+import org.jboss.windup.reporting.model.ClassificationModel;
 import org.jboss.windup.reporting.model.TechnologyTagLevel;
 import org.jboss.windup.reporting.service.ClassificationService;
 import org.jboss.windup.reporting.service.TechnologyTagService;
@@ -21,6 +25,8 @@ import org.ocpsoft.rewrite.context.EvaluationContext;
  * Discovers WebSphere EJB Extension XML files and parses the related metadata
  *
  * @author <a href="mailto:bradsdavis@gmail.com">Brad Davis</a>
+ * @author <a href="mailto:mnovotny@redhat.com">Marek Novotny</a>
+ * 
  */
 @RuleMetadata(phase = InitialAnalysisPhase.class, after = DiscoverEjbConfigurationXmlRuleProvider.class, perform = "Discover WebSphere EJB XML Files")
 public class ResolveWebSphereEjbExtensionXmlRuleProvider extends IteratingRuleProvider<XmlFileModel>
@@ -36,8 +42,33 @@ public class ResolveWebSphereEjbExtensionXmlRuleProvider extends IteratingRulePr
     public void perform(GraphRewrite event, EvaluationContext context, XmlFileModel payload)
     {
         ClassificationService classificationService = new ClassificationService(event.getGraphContext());
-        classificationService.attachClassification(event, context, payload, IssueCategoryRegistry.MANDATORY, "WebSphere EJB Ext", "WebSphere Enterprise Java Bean Extension XML Descriptor.");
+        
+        ClassificationModel classificationModel = classificationService.attachClassification(event, context, payload, IssueCategoryRegistry.MANDATORY,
+                    "WebSphere EJB extension descriptor (ibm-ejb-jar-ext)",
+                    "WebSphere Enterprise Java Bean Extension XML Descriptor is used to specify extensions to be (de-)activated in the EJB Container."
+                                + " \n "
+                                + "JBoss EAP uses Java EE `jboss-ejb.xml` file descriptor or EAP specific `jboss-ejb3.xml` descriptor file. EJB 3.2 doesn't require descriptor file to be in deployment.");
+        classificationModel.setEffort(3);
+        
+        GraphContext graphContext = event.getGraphContext();
+        LinkService linkService = new LinkService(graphContext);
+        
+        LinkModel link = linkService.create();
+        link.setDescription("Websphere AS - EJB 3.0 application bindings overview");
+        link.setLink("https://www.ibm.com/support/knowledgecenter/en/SSAW57_7.0.0/com.ibm.websphere.nd.doc/info/ae/ae/cejb_bindingsejbfp.html");
 
+        classificationService.attachLink(classificationModel, link);
+        
+        LinkModel ejb3RefLink = linkService.create();
+        ejb3RefLink.setDescription("EAP 7 - jboss-ejb3.xml Deployment Descriptor Reference");
+        ejb3RefLink.setLink("https://access.redhat.com/documentation/en-us/red_hat_jboss_enterprise_application_platform/7.0/html-single/developing_ejb_applications/#jboss_ejb3_xml_deployment_descriptor_reference");
+        classificationService.attachLink(classificationModel, ejb3RefLink);
+        
+        LinkModel ejb3RefLink_ = linkService.create();
+        ejb3RefLink_.setDescription("EAP 6 - jboss-ejb3.xml Deployment Descriptor Reference");
+        ejb3RefLink_.setLink("https://access.redhat.com/documentation/en-US/JBoss_Enterprise_Application_Platform/6/html-single/Development_Guide/index.html#jboss-ejb3xml_Deployment_Descriptor_Reference");
+        classificationService.attachLink(classificationModel, ejb3RefLink_);
+        
         TechnologyTagService technologyTagService = new TechnologyTagService(event.getGraphContext());
         technologyTagService.addTagToFileModel(payload, "WebSphere EJB Ext", TechnologyTagLevel.IMPORTANT);
 
