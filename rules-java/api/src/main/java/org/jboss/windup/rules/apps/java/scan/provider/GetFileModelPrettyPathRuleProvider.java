9f1c7966bd8b8ff7a8da40c4efd639aaa2d9821b
David Klingenberg <dklingen@redhat.com>
2017-08-16 12:07:20 +0200

    Keep working code

--- rules-java/api/src/main/java/org/jboss/windup/rules/apps/java/scan/provider/GetFileModelPrettyPathRuleProvider.java
+++ rules-java/api/src/main/java/org/jboss/windup/rules/apps/java/scan/provider/GetFileModelPrettyPathRuleProvider.java
@@ -35,7 +35,7 @@ public class GetFileModelPrettyPathRuleProvider extends AbstractRuleProvider
                     .when(Query.fromType(FileModel.class))
                     .perform(new GetFileModelPrettyPathRuleProvider.GetPrettyPathIterationOperator()
                                 .and(Commit.every(100))
-                                .and(IterationProgress.monitoring("Index Java Source Files", 250)));
+                                .and(IterationProgress.monitoring("Getting prettyPath.....", 250)));
     }
 
     private final class GetPrettyPathIterationOperator extends AbstractIterationOperation<FileModel>
@@ -50,16 +50,29 @@ public class GetFileModelPrettyPathRuleProvider extends AbstractRuleProvider
         {
             try
             {
+                /**
+                 * @note: This code is extremely fragile. Sometimes for no real reason, it calls
+                 *  getPrettyPathWithinProject on 'FileModel' instead of correct overridden 'JavaSourceFileModel' method.
+                 *
+                 *  Commented out code below is kept here for potential debugging of this issue.
+                 */
                 payload.setCachedPrettyPath(payload.getPrettyPathWithinProject(true));
 
+                /*
                 String prettyPath = payload.getCachedPrettyPath();
-                LOG.info(prettyPath);
+                LOG.info("Pretty path after payload.getPrettyPath: " + prettyPath);
+                */
+
                 GraphContext graphContext = event.getGraphContext();
                 GetPrettyPathForFile.addPrettyPathToModel(payload, graphContext);
-                LOG.info(prettyPath);
+
+                /*
+                prettyPath = payload.getCachedPrettyPath();
+                LOG.info("Pretty path after GetPrettyPathForFile.addPrettyPathToModel: " + prettyPath);
 
                 Object id = payload.asVertex().getId();
-                LOG.info(id.toString());
+                LOG.info("Vertex id: " + id.toString());
+                */
             }
             catch (Exception e)
             {
