75fa2bcba2175738b854be8eab77523735f3580a
Jess Sightler <jesse.sightler@gmail.com>
2017-09-08 17:54:56 -0400

    QueryHandler: Specify some basic graph queries via XML

---
+++ config-xml/addon/src/main/java/org/jboss/windup/config/parser/xml/when/QueryHandler.java
@@ -0,0 +1,80 @@
+package org.jboss.windup.config.parser.xml.when;
+
+import static org.joox.JOOX.$;
+
+import javax.inject.Inject;
+
+import org.apache.commons.lang3.StringUtils;
+import org.jboss.windup.config.exception.ConfigurationException;
+import org.jboss.windup.config.parser.ElementHandler;
+import org.jboss.windup.config.parser.NamespaceElementHandler;
+import org.jboss.windup.config.parser.ParserContext;
+import org.jboss.windup.config.parser.xml.RuleProviderHandler;
+import org.jboss.windup.config.query.Query;
+import org.jboss.windup.graph.GraphTypeManager;
+import org.jboss.windup.graph.model.WindupFrame;
+import org.jboss.windup.graph.model.WindupVertexFrame;
+import org.jboss.windup.util.exception.WindupException;
+import org.w3c.dom.Element;
+
+/**
+ * @author <a href="mailto:jesse.sightler@gmail.com">Jesse Sightler</a>
+ */
+@NamespaceElementHandler(elementName = QueryHandler.ELEMENT_NAME, namespace = RuleProviderHandler.WINDUP_RULE_NAMESPACE)
+public class QueryHandler implements ElementHandler<Query>
+{
+    public static final String ELEMENT_NAME = "graph-query";
+    public static final String DISCRIMINATOR = "discriminator";
+    public static final String FROM = "from";
+    public static final String AS = "as";
+
+    @Inject
+    private GraphTypeManager graphTypeManager;
+
+    @Override
+    public Query processElement(ParserContext handlerManager, Element element) throws ConfigurationException
+    {
+        String from = $(element).attr(FROM);
+        String as = $(element).attr(AS);
+        String discriminator = $(element).attr(DISCRIMINATOR);
+        if (StringUtils.isBlank(discriminator))
+        {
+            throw new WindupException("Error loading rule, '" + DISCRIMINATOR + "' attribute must be specified!");
+        }
+
+        Query query = null;
+        if (StringUtils.isNotBlank(from))
+        {
+            query = (Query) Query.from(from);
+        }
+
+        Class<? extends WindupFrame> typeAsWindupFrame = this.graphTypeManager.getTypeForDiscriminator(discriminator);
+        if (typeAsWindupFrame == null)
+            throw new WindupException("Error, type: " + discriminator + " not registered!");
+
+        Class<? extends WindupVertexFrame> type = cast(typeAsWindupFrame);
+        if (query == null)
+        {
+            query = (Query)Query.fromType(type);
+        }
+        else
+        {
+            query = (Query)query.includingType(type);
+        }
+
+        if (StringUtils.isNotBlank(as))
+            query.as(as);
+
+        return query;
+    }
+
+    @SuppressWarnings("unchecked")
+    private Class<? extends WindupVertexFrame> cast(Class<? extends WindupFrame> typeAsWindupFrame)
+    {
+        if (!WindupVertexFrame.class.isAssignableFrom(typeAsWindupFrame))
+            throw new WindupException("Only types that are a subclass of " + WindupVertexFrame.class.getCanonicalName() + " are supported by the "
+                    + ELEMENT_NAME + " element!");
+
+        return (Class<? extends WindupVertexFrame>) typeAsWindupFrame;
+    }
+}
---
+++ config-xml/tests/src/test/java/org/jboss/windup/config/parser/xml/when/QueryHandlerTest.java
@@ -0,0 +1,71 @@
+package org.jboss.windup.config.parser.xml.when;
+
+import static org.joox.JOOX.$;
+
+import java.io.File;
+import java.util.Collections;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.forge.arquillian.AddonDependency;
+import org.jboss.forge.arquillian.AddonDependencies;
+import org.jboss.forge.arquillian.archive.AddonArchive;
+import org.jboss.forge.furnace.Furnace;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.windup.config.loader.RuleLoaderContext;
+import org.jboss.windup.config.parser.ParserContext;
+import org.jboss.windup.config.query.Query;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+@RunWith(Arquillian.class)
+public class QueryHandlerTest
+{
+    private static final String XML_RHAMT_FILE = "src/test/resources/query-handler.rhamt.xml";
+
+    @Deployment
+    @AddonDependencies({
+            @AddonDependency(name = "org.jboss.windup.config:windup-config"),
+            @AddonDependency(name = "org.jboss.windup.rules.apps:windup-rules-java"),
+            @AddonDependency(name = "org.jboss.windup.rules.apps:windup-rules-xml"),
+            @AddonDependency(name = "org.jboss.windup.config:windup-config-xml"),
+            @AddonDependency(name = "org.jboss.forge.furnace.container:cdi")
+    })
+    public static AddonArchive getDeployment()
+    {
+        final AddonArchive archive = ShrinkWrap.create(AddonArchive.class)
+                .addBeansXML();
+
+        return archive;
+    }
+
+    @Inject
+    private Furnace furnace;
+
+    @Test
+    public void testQueryHandler() throws Exception
+    {
+        File fXmlFile = new File(XML_RHAMT_FILE);
+        RuleLoaderContext loaderContext = new RuleLoaderContext(Collections.singleton(fXmlFile.toPath()), null);
+        ParserContext parser = new ParserContext(furnace, loaderContext);
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        dbFactory.setNamespaceAware(true);
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.parse(fXmlFile);
+        List<Element> queryElementList = $(doc).children(QueryHandler.ELEMENT_NAME).get();
+
+        Element firstQueryElement = queryElementList.get(0);
+        Query query = parser.<Query> processElement(firstQueryElement);
+
+        Assert.assertEquals("test-from", query.getInputVariablesName());
+        Assert.assertEquals("test-as", query.getOutputVariablesName());
+    }
+}
