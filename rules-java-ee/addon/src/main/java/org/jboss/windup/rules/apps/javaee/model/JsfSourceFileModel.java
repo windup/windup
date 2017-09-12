a28118bdd13c5bceda8645764b1cb8f3ed7a569a
Jess Sightler <jesse.sightler@gmail.com>
2017-09-12 16:59:17 -0400

    WINDUPRULE-284: Statistics Rules: View:Web and View:Rich, jsf, jsp, jnlp, applet, etc

---
+++ rules-java-ee/addon/src/main/java/org/jboss/windup/rules/apps/javaee/model/JsfSourceFileModel.java
@@ -0,0 +1,18 @@
+package org.jboss.windup.rules.apps.javaee.model;
+
+import org.jboss.windup.rules.apps.java.model.AbstractJavaSourceModel;
+
+import com.tinkerpop.frames.modules.typedgraph.TypeValue;
+
+/**
+ * Represents a JSF template (either in jsp syntax or jsf syntax).
+ *
+ * @author <a href="mailto:jesse.sightler@gmail.com">Jesse Sightler</a>
+ */
+@TypeValue(JsfSourceFileModel.TYPE)
+public interface JsfSourceFileModel extends AbstractJavaSourceModel
+{
+    String TYPE = "JsfSourceFile";
+
+
+}
