38dce3bab83d43ec67e24c269565815465516788
mrizzi <mrizzi@redhat.com>
2017-09-13 15:05:50 +0200

    WINDUPRULE-286 Models to handle markup technologies

---
+++ rules-java-ee/addon/src/main/java/org/jboss/windup/rules/apps/javaee/model/CssFileModel.java
@@ -0,0 +1,15 @@
+package org.jboss.windup.rules.apps.javaee.model;
+
+import com.tinkerpop.frames.modules.typedgraph.TypeValue;
+import org.jboss.windup.graph.model.resource.FileModel;
+import org.jboss.windup.graph.model.resource.SourceFileModel;
+
+import java.util.logging.Logger;
+
+@TypeValue(CssFileModel.TYPE)
+public interface CssFileModel extends FileModel, SourceFileModel
+{
+    Logger LOG = Logger.getLogger(CssFileModel.class.getName());
+
+    String TYPE = "CssFile";
+}
