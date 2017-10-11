5b93de4d1d84c7bb8f41355e976890fa8be52f45
mrizzi <mrizzi@redhat.com>
2017-10-11 14:21:30 +0200

    WINDUP-1732 <annotation-list> w/o 'name' attribute

--- rules-java/tests/src/test/java/org/jboss/windup/rules/java/JavaClassAnnotationFilteringTest.java
+++ rules-java/tests/src/test/java/org/jboss/windup/rules/java/JavaClassAnnotationFilteringTest.java
@@ -122,6 +122,7 @@ public class JavaClassAnnotationFilteringTest
             processor.execute(processorConfig);
 
             Assert.assertEquals(1, complexProvider.baseRuleHitCount);
+            Assert.assertEquals(1, complexProvider.nestedAnnotationHitCount);
         }
     }
 
@@ -190,6 +191,7 @@ public class JavaClassAnnotationFilteringTest
     public static class ComplexAnnotationScanProvider extends AbstractRuleProvider
     {
         private int baseRuleHitCount = 0;
+        private int nestedAnnotationHitCount = 0;
 
         public ComplexAnnotationScanProvider()
         {
@@ -214,6 +216,21 @@ public class JavaClassAnnotationFilteringTest
                         public void perform(GraphRewrite event, EvaluationContext context, JavaTypeReferenceModel payload)
                         {
                             baseRuleHitCount++;
+                        }
+                    })
+                    .addRule().when(
+                            JavaClass.references("javax.annotation.sql.DataSourceDefinitions")
+                                    .at(TypeReferenceLocation.ANNOTATION)
+                                    .annotationMatches(
+                                            "value",
+                                            new AnnotationListCondition(0).addCondition(new AnnotationTypeCondition("javax.annotation.sql.DataSourceDefinition"))
+                                    )
+                    ).perform(new AbstractIterationOperation<JavaTypeReferenceModel>()
+                    {
+                        @Override
+                        public void perform(GraphRewrite event, EvaluationContext context, JavaTypeReferenceModel payload)
+                        {
+                            nestedAnnotationHitCount++;
                         }
                     });
         }
