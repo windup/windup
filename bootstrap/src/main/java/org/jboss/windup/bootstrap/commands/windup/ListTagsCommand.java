ce7a2fd2c23c2af3679bcaeae38e3e169e6bd60b
Marco Rizzi <mrizzi@users.noreply.github.com>
2017-11-10 19:03:27 +0100

    WINDUP-1758 WINDUP-1768 CLI  option in list* commands (#1219) (#1230)
    * WINDUP-1758 CLI  option in list* commands
    
    * WINDUP-1758 CLI option in list* commands: improved implementation

--- bootstrap/src/main/java/org/jboss/windup/bootstrap/commands/windup/ListTagsCommand.java
+++ bootstrap/src/main/java/org/jboss/windup/bootstrap/commands/windup/ListTagsCommand.java
@@ -1,27 +1,50 @@
 package org.jboss.windup.bootstrap.commands.windup;
 
-import org.jboss.windup.bootstrap.commands.AbstractListCommandWithoutFurnace;
+import org.jboss.windup.bootstrap.commands.AbstractListCommand;
 import org.jboss.windup.bootstrap.commands.Command;
 import org.jboss.windup.bootstrap.commands.CommandPhase;
 import org.jboss.windup.bootstrap.commands.CommandResult;
+import org.jboss.windup.bootstrap.commands.FurnaceDependent;
+import org.jboss.windup.config.metadata.RuleProviderRegistryCache;
 import org.jboss.windup.exec.configuration.options.IncludeTagsOption;
+
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Set;
 
 /**
  * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
  */
-public class ListTagsCommand extends AbstractListCommandWithoutFurnace implements Command
+public class ListTagsCommand extends AbstractListCommand implements Command, FurnaceDependent
 {
+    private final List<Path> userProvidedPaths;
+
+    public ListTagsCommand(List<String> arguments)
+    {
+        userProvidedPaths = getUserProvidedPaths(arguments);
+    }
+
     @Override
     public CommandResult execute()
     {
-        printValuesSorted("Available tags", getOptionValuesFromHelp(IncludeTagsOption.NAME));
+        Set<String> result;
+        if (userProvidedPaths.isEmpty())
+        {
+            result = getOptionValuesFromHelp(IncludeTagsOption.NAME);
+        } else
+        {
+            RuleProviderRegistryCache ruleProviderRegistryCache = getFurnace().getAddonRegistry().getServices(RuleProviderRegistryCache.class).get();
+            userProvidedPaths.forEach(userProvidedPath -> ruleProviderRegistryCache.addUserRulesPath(userProvidedPath));
+            result = ruleProviderRegistryCache.getAvailableTags();
+        }
+        printValuesSorted("Available tags", result);
         return CommandResult.EXIT;
     }
 
     @Override
     public CommandPhase getPhase()
     {
-        return CommandPhase.PRE_CONFIGURATION;
+        return userProvidedPaths.isEmpty() ? CommandPhase.PRE_CONFIGURATION : CommandPhase.PRE_EXECUTION;
     }
 
 }
