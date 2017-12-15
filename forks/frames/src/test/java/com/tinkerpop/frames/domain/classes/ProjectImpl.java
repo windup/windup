4f28497a2c8443c86419746703d61b1a42740ec9
Jess Sightler <jesse.sightler@gmail.com>
2017-12-14 22:20:23 -0500

    WINDUP-1542: Replace Tinkerpop 2 Dependencies

--- forks/frames/src/test/java/com/tinkerpop/frames/domain/classes/ProjectImpl.java
+++
@@ -1,9 +0,0 @@
-package com.tinkerpop.frames.domain.classes;
-
-public abstract class ProjectImpl implements Project {
-	@Override
-	public String getLanguageUsingMixin() {
-	
-		return getLanguage();
-	}
-}
