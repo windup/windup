dad8699d54c50b9491f86e14227eb4b84964c22a
jonathan <jvilalop@redhat.com>
2018-10-18 17:41:13 +0200

    WINDUPRULE-142 Added JaxRSWebServiceModel in order to allow Jenkins to compile because of the dependent projects

---
+++ rules-java-ee/addon/src/main/java/org/jboss/windup/rules/apps/javaee/model/JaxRSWebServiceModel.java
@@ -0,0 +1,62 @@
+package org.jboss.windup.rules.apps.javaee.model;
+
+import org.apache.tinkerpop.gremlin.structure.Direction;
+import org.jboss.windup.graph.Adjacency;
+import org.jboss.windup.graph.Indexed;
+import org.jboss.windup.graph.Property;
+import org.jboss.windup.graph.model.TypeValue;
+import org.jboss.windup.rules.apps.java.model.JavaClassModel;
+
+/**
+ * JaxRS REST Web Service.
+ * 
+ * @author <a href="mailto:bradsdavis@gmail.com">Brad Davis</a>
+ */
+@TypeValue(JaxRSWebServiceModel.TYPE)
+public interface JaxRSWebServiceModel extends WebServiceModel
+{
+    String TYPE = "JaxRSWebServiceModel";
+    String JAXRS_IMPLEMENTATION_CLASS = "jaxrsImplementationClass";
+    String JAXRS_INTERFACE = "jaxrsInterface";
+    
+    String PATH = "jaxrsPath";
+    String SOURCE = "source";
+    
+
+    /**
+     * Contains the URL path to the JaxRS service.
+     */
+    @Property(PATH)
+    @Indexed
+    String getPath();
+
+    /**
+     * Contains the URL path to the JaxRS service.
+     */
+    @Property(PATH)
+    void setPath(String packageName);
+    
+    /**
+     * Contains the JAX-RS implementation class
+     */
+    @Adjacency(label = JAXRS_IMPLEMENTATION_CLASS, direction = Direction.OUT)
+    void setImplementationClass(JavaClassModel implRef);
+
+    /**
+     * Contains the JAX-RS implementation class
+     */
+    @Adjacency(label = JAXRS_IMPLEMENTATION_CLASS, direction = Direction.OUT)
+    JavaClassModel getImplementationClass();
+
+    /**
+     * Contains the JAX-RS implementation class
+     */
+    @Adjacency(label = JAXRS_INTERFACE, direction = Direction.OUT)
+    void setInterface(JavaClassModel interfaceRef);
+
+    /**
+     * Contains the JAX-RS implementation class
+     */
+    @Adjacency(label = JAXRS_INTERFACE, direction = Direction.OUT)
+    JavaClassModel getInterface();
+}
