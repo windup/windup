9b9703eef51c67f73df9530fa608ea6a9c362210
jonathan <jvilalop@redhat.com>
2018-10-18 19:24:27 +0200

    winduprule-142 organised imports

--- rules-java-ee/tests/src/test/java/org/jboss/windup/rules/apps/javaee/rules/DiscoverSpringRestServicesRuleProviderTest.java
+++ rules-java-ee/tests/src/test/java/org/jboss/windup/rules/apps/javaee/rules/DiscoverSpringRestServicesRuleProviderTest.java
@@ -1,11 +1,7 @@
 package org.jboss.windup.rules.apps.javaee.rules;
 
 import org.apache.commons.io.FileUtils;
-import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.jboss.windup.config.KeepWorkDirsOption;
 import org.jboss.windup.exec.WindupProcessor;
 import org.jboss.windup.exec.configuration.WindupConfiguration;
@@ -17,20 +13,16 @@ import org.jboss.windup.graph.service.GraphService;
 import org.jboss.windup.rules.apps.java.config.SourceModeOption;
 import org.jboss.windup.rules.apps.javaee.AbstractTest;
 import org.jboss.windup.rules.apps.javaee.model.RestWebServiceModel;
-import org.jboss.windup.rules.apps.javaee.model.SpringRemoteServiceModel;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import javax.inject.Inject;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.UUID;
-
-import static org.junit.Assert.*;
 
 @RunWith(Arquillian.class)
 public class DiscoverSpringRestServicesRuleProviderTest extends AbstractTest {
