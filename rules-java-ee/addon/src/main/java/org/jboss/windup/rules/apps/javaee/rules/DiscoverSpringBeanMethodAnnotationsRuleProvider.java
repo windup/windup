bcc0f596a392b19f2cac517055f8a15c9558f2b6
jonathan <jvilalop@redhat.com>
2019-04-04 12:38:34 +0200

    winduprule-363-142 added null check in stream

--- rules-java-ee/addon/src/main/java/org/jboss/windup/rules/apps/javaee/rules/DiscoverSpringBeanMethodAnnotationsRuleProvider.java
+++ rules-java-ee/addon/src/main/java/org/jboss/windup/rules/apps/javaee/rules/DiscoverSpringBeanMethodAnnotationsRuleProvider.java
@@ -92,7 +92,7 @@ public class DiscoverSpringBeanMethodAnnotationsRuleProvider extends AbstractRul
     private Optional<JavaClassModel> getImplementationJavaClassModelFromInterface(GraphRewrite event, String returnType) {
         //with that interface we will seach in the next lines the first class implementing that interface
         JavaClassService javaClassService = new JavaClassService(event.getGraphContext());
-        Optional<JavaClassModel> returnTypeJavaClassModel = javaClassService.findAll().stream().filter(e -> e.getQualifiedName().contains(returnType)).findFirst();
+        Optional<JavaClassModel> returnTypeJavaClassModel = javaClassService.findAll().stream().filter(e -> e.getQualifiedName() != null && e.getQualifiedName().contains(returnType)).findFirst();
         if (returnTypeJavaClassModel.isPresent() && returnTypeJavaClassModel.get().isInterface()) {
             return javaClassService.findAll().stream()
                     .filter(e -> e.getInterfaces()
