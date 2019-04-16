cebecd5ff44aaadd90632c83a2e03bad3cd6c61a
m-brophy <mbrophy@redhat.com>
2019-04-16 11:48:19 +0100

    WINDUP-2351: Application and Tags CSV report: test class enhanced

--- tests/src/test/java/org/jboss/windup/tests/bootstrap/migrate/ExportCsvTest.java
+++ tests/src/test/java/org/jboss/windup/tests/bootstrap/migrate/ExportCsvTest.java
@@ -5,15 +5,21 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.assertTrue;
 
 public class ExportCsvTest extends AbstractBootstrapTestWithRules {
     @Rule
     public final TemporaryFolder tmp = new TemporaryFolder();
+    private final String DELIMITER = ",";
 
     @Test
     public void exportCsv() throws IOException {
@@ -33,5 +39,40 @@ public class ExportCsvTest extends AbstractBootstrapTestWithRules {
 
         File appTagsCsv = new File(tmp.getRoot(), "ApplicationFileTechnologies.csv");
         assertTrue(appTagsCsv.exists());
+
+        List<List<String>> records = readCSVFile(appTagsCsv);
+        records.forEach(record ->
+                {
+                   assertTrue(isRowSorted(record));
+                }
+        );
+    }
+
+    private List<List<String>> readCSVFile(File csvFile) throws IOException
+    {
+        List<List<String>> rows = new ArrayList<>();
+        try (BufferedReader buffer = new BufferedReader(new FileReader(csvFile))) {
+            String lineString;
+            while ((lineString = buffer.readLine()) != null) {
+                String[] columns = lineString.split(DELIMITER);
+                rows.add(Arrays.asList(columns));
+            }
+        }
+
+        return rows;
+    }
+
+    public boolean isRowSorted(List<String> row)
+    {
+        //This loop ignores the first entry in the list (ie where i=0) which is the app name,
+        //only the entries after that are sorted
+        for(int i=1;i<row.size()-1;i++)
+        {
+            if(row.get(i).compareTo(row.get(i+1))>0)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
