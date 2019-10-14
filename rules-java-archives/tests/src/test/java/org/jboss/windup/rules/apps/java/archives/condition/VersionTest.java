e45a4541179a0151a367902834bcc72bcfe43ec5
mrizzi <mrizzi@redhat.com>
2019-10-14 12:03:49 +0200

    WINDUP-2457 Add inclusive check in test

--- rules-java-archives/tests/src/test/java/org/jboss/windup/rules/apps/java/archives/condition/VersionTest.java
+++ rules-java-archives/tests/src/test/java/org/jboss/windup/rules/apps/java/archives/condition/VersionTest.java
@@ -9,8 +9,14 @@ public class VersionTest {
         Version version = Version.fromVersion("1.2.3").toVersion("3.2.1");
         Assert.assertEquals(true, version.validate("2.2"));
         Assert.assertEquals(true, version.validate("2.2.2.Final"));
+        Assert.assertEquals(true, version.validate("1.2.3"));
+        Assert.assertEquals(true, version.validate("3.2.1.Final"));
         version = Version.fromVersion("1.0.0.RELEASE").toVersion("2.1.1.RELEASE");
         Assert.assertEquals(true, version.validate("1.2.7.RELEASE"));
         Assert.assertEquals(false, version.validate("2.2.7.RELEASE"));
+        Assert.assertEquals(true, version.validate("1.0.0.RELEASE"));
+        Assert.assertEquals(true, version.validate("1.0.0.Final"));
+        Assert.assertEquals(true, version.validate("2.1.1.RELEASE"));
+        Assert.assertEquals(true, version.validate("2.1.1"));
     }
 }
