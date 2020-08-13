e41e16d4c0c5c9d6cccbd3e2677aa58f079edae1
John Steele <josteele@redhat.com>
2020-08-13 13:54:58 -0400

    WINDUP-2688 - add support for remainder of mta-cli options to tooling… (#1392)
    * WINDUP-2688 - add support for remainder of mta-cli options to toolingMode
    
    * WINDUP-2688 - add support for remainder of mta-cli options to toolingMode (fix assertion)

--- tooling/api/src/main/java/org/jboss/windup/tooling/IOptionKeys.java
+++ tooling/api/src/main/java/org/jboss/windup/tooling/IOptionKeys.java
@@ -8,9 +8,26 @@ public interface IOptionKeys
 	public static final String TARGET = "target";
 	public static final String INPUT = "input";
 	public static final String OUTPUT = "output";
-	public static final String PACKAGES = "packages";
-	public static final String IGNORE_PATH = "userIgnorePath";
 	public static final String IGNORE_PATTERN = "ignorePattern";
 	public static final String HOME = "windupHome";
 	public static final String CUSTOM_RULES_DIR = "userRulesDirectory";
+
+	public static final String USER_IGNORE_PATH= "userIgnorePath";
+	public static final String OVERWRITE = "overwrite";
+	public static final String EXCLUDE_PACKAGES = "excludePackages";
+	public static final String MAVENIZE_GROUP_ID = "mavenizeGroupId";
+	public static final String EXPORT_CSV = "exportCSV";
+	public static final String EXCLUDE_TAGS = "excludeTags";
+	public static final String PACKAGES = "packages";
+	public static final String ADDITIONAL_CLASSPATH = "additionalClasspath";
+	public static final String DISABLE_TATTLETALE = "disableTattletale";
+	public static final String ENABLE_COMPATIBLE_FILES_REPORT = "enableCompatibleFilesReport";
+	public static final String INCLUDE_TAGS = "includeTags";
+	public static final String ONLINE = "online";
+	public static final String ENABLE_CLASS_NOT_FOUND_ANALYSIS = "enableClassNotFoundAnalysis";
+	public static final String ENABLE_TATTLETALE = "enableTattletale";
+	public static final String EXPLODED_APP = "explodedApp";
+	public static final String KEEP_WORK_DIRS = "keepWorkDirs";
+	public static final String MAVENIZE = "mavenize";
+	public static final String INPUT_APPLICATION_NAME = "inputApplicationName";
 }
