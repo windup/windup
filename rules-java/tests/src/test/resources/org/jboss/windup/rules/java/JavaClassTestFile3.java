5f28d16ca47a9e369ff9841881d450ae7bdef785
Juan Manuel Leflet Estrada <jleflete@redhat.com>
2022-03-11 10:19:46 +0100

    [WINDUP-3284] Inconsistency between JDK8 and JDK11 when analyzing certain java classes (#1459)
    * [WINDUP-3284] Add failing test for getPeer method
    
    * [WINDUP-3284] Add failing test for getPeer method
    
    * WINDUP-3284 Removed JVM Boot classpath and added additional classpath in test
    
    * WINDUP-3284 Provided a Windup ASTParser to change the classpaths management
    
    Co-authored-by: mrizzi <mrizzi@redhat.com>

---
+++ rules-java/tests/src/test/resources/org/jboss/windup/rules/java/JavaClassTestFile3.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+public class JavaClassTestFile3 {
+    public static void main(String[] args) {
+        java.awt.peer.FontPeer peer = new java.awt.Font("Sans", 1, 1).getPeer();
+        Font testFont=new Font("Sans", 1, 1);
+        testFont.getFontName();
+        java.awt.peer.FontPeer peer1 = testFont.getPeer();
+        System.err.println(peer + " [java.awt.peer] is really internal, should not be used");
+    }
+}
