55d7df98cb7b5998f23744a4a2581e2331bc17dc
Aki Tozawa <atozawa@jp.ibm.com>
2022-03-17 14:39:07 +0900

    Refactoring stack-trace model

--- rules-java-diva/addon/src/main/java/org/jboss/windup/rules/apps/diva/service/DivaStackTraceService.java
+++ rules-java-diva/addon/src/main/java/org/jboss/windup/rules/apps/diva/service/DivaStackTraceService.java
@@ -1,27 +1,25 @@
 package org.jboss.windup.rules.apps.diva.service;
 
+import java.util.List;
+
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__;
 import org.jboss.windup.graph.GraphContext;
 import org.jboss.windup.graph.model.FileLocationModel;
 import org.jboss.windup.graph.model.FileReferenceModel;
 import org.jboss.windup.graph.model.resource.FileModel;
-import org.jboss.windup.graph.service.FileService;
+import org.jboss.windup.graph.service.FileLocationService;
 import org.jboss.windup.graph.service.GraphService;
 import org.jboss.windup.rules.apps.diva.model.DivaStackTraceModel;
 import org.jboss.windup.rules.apps.java.model.JavaMethodModel;
 
 public class DivaStackTraceService extends GraphService<DivaStackTraceModel> {
 
-    FileService fileService;
+    FileLocationService fileLocationService;
 
     public DivaStackTraceService(GraphContext context) {
         super(context, DivaStackTraceModel.class);
-        fileService = new FileService(context);
-    }
-
-    public void setFilePath(DivaStackTraceModel model, String filePath) {
-        model.setFile(fileService.createByFilePath(filePath));
+        fileLocationService = new FileLocationService(context);
     }
 
 //    static int count = 0;
@@ -30,22 +28,37 @@ public class DivaStackTraceService extends GraphService<DivaStackTraceModel> {
     public DivaStackTraceModel getOrCreate(FileModel fileModel, int lineNumber, int columnNumber, int length,
             DivaStackTraceModel parent, JavaMethodModel method) {
         // FileModel fileModel = fileService.createByFilePath(filePath);
-        GraphTraversal<?, ?> traversal = getQuery().getRawTraversal().has(FileLocationModel.COLUMN_NUMBER, columnNumber)
-                .has(FileLocationModel.LINE_NUMBER, lineNumber).has(FileLocationModel.LENGTH, length)
+        GraphTraversal<?, ?> traversal = getGraphContext().getQuery(FileLocationModel.class).getRawTraversal()
+                .has(FileLocationModel.COLUMN_NUMBER, columnNumber).has(FileLocationModel.LINE_NUMBER, lineNumber)
+                .has(FileLocationModel.LENGTH, length)
                 .filter(__.out(FileReferenceModel.FILE_MODEL).is(fileModel.getElement()));
-        if (parent == null) {
-            traversal = traversal.not(__.out(DivaStackTraceModel.PARENT));
+
+        List<?> locs = traversal.toList();
+
+        FileLocationModel location;
+        DivaStackTraceModel model = null;
+        if (locs.isEmpty()) {
+            location = fileLocationService.create();
+            location.setColumnNumber(columnNumber);
+            location.setLineNumber(lineNumber);
+            location.setLength(length);
+            location.setFile(fileModel);
+
         } else {
-            traversal = traversal.filter(__.out(DivaStackTraceModel.PARENT).is(parent.getElement()));
+            location = (FileLocationModel) locs.get(0);
+            traversal = getQuery().getRawTraversal().has(DivaStackTraceModel.LOCATION, location.getElement());
+            if (parent == null) {
+                traversal = traversal.not(__.out(DivaStackTraceModel.PARENT));
+            } else {
+                traversal = traversal.filter(__.out(DivaStackTraceModel.PARENT).is(parent.getElement()));
+            }
+            model = getUnique(traversal);
         }
-       DivaStackTraceModel model = getUnique(traversal);
-//        }
+
         if (model == null) {
             model = create();
-            model.setColumnNumber(columnNumber);
-            model.setLineNumber(lineNumber);
-            model.setLength(length);
-            model.setFile(fileModel);
+            model.setLocation(location);
+
             if (parent != null) {
                 model.setParent(parent);
             }
