d7c0b98d6c1bc5c0163c4973e0141f8999132187
carlosthe19916 <2582866+carlosthe19916@users.noreply.github.com>
2023-01-25 09:09:31 +0100

    Add transactions

---
+++ reporting-data/addon/src/main/java/org/jboss/windup/reporting/data/rules/ApplicationTransactionsRuleProvider.java
@@ -0,0 +1,125 @@
+package org.jboss.windup.reporting.data.rules;
+
+import org.jboss.windup.config.GraphRewrite;
+import org.jboss.windup.config.metadata.RuleMetadata;
+import org.jboss.windup.config.phase.ReportRenderingPhase;
+import org.jboss.windup.graph.GraphContext;
+import org.jboss.windup.graph.model.ProjectModel;
+import org.jboss.windup.graph.model.WindupConfigurationModel;
+import org.jboss.windup.graph.model.resource.FileModel;
+import org.jboss.windup.graph.service.GraphService;
+import org.jboss.windup.graph.service.WindupConfigurationService;
+import org.jboss.windup.reporting.data.dto.ApplicationTransactionsDto;
+import org.jboss.windup.reporting.model.WindupVertexListModel;
+import org.jboss.windup.reporting.service.SourceReportService;
+import org.jboss.windup.rules.apps.diva.model.DivaContextModel;
+import org.jboss.windup.rules.apps.diva.model.DivaEntryMethodModel;
+import org.jboss.windup.rules.apps.diva.model.DivaSqlOpModel;
+import org.jboss.windup.rules.apps.java.model.JavaClassModel;
+import org.jboss.windup.rules.apps.java.service.JavaClassService;
+import org.jboss.windup.rules.apps.javaee.service.SpringBeanService;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@RuleMetadata(
+        phase = ReportRenderingPhase.class,
+        haltOnException = true
+)
+public class ApplicationTransactionsRuleProvider extends AbstractApiRuleProvider {
+
+    @Override
+    public String getBasePath() {
+        return "transactions";
+    }
+
+    @Override
+    public Object getAll(GraphRewrite event) {
+        GraphContext context = event.getGraphContext();
+        SourceReportService sourceReportService = new SourceReportService(context);
+        JavaClassService javaClassService = new JavaClassService(context);
+
+        GraphService<DivaContextModel> cxtModelService = new GraphService<>(context, DivaContextModel.class);
+        List<DivaContextModel> cxts = cxtModelService.findAll();
+        if (cxts.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        WindupConfigurationModel configurationModel = WindupConfigurationService.getConfigurationModel(context);
+        GraphService<WindupVertexListModel> listService = new GraphService<>(context, WindupVertexListModel.class);
+        WindupVertexListModel<DivaContextModel> windupVertexListModel = listService.create();
+        windupVertexListModel.addAll(cxts);
+
+        List<ApplicationTransactionsDto> result = new ArrayList<>();
+
+        for (FileModel inputPath : configurationModel.getInputPaths()) {
+            ProjectModel application = inputPath.getProjectModel();
+
+            ApplicationTransactionsDto applicationTransactionsDto = new ApplicationTransactionsDto();
+            result.add(applicationTransactionsDto);
+
+            applicationTransactionsDto.applicationId = application.getId().toString();
+            applicationTransactionsDto.transactions = StreamSupport.stream(windupVertexListModel.spliterator(), false)
+                    .map(divaContextModel -> {
+
+                        ApplicationTransactionsDto.TransactionDto transactionDto = divaContextModel.getConstraints().stream()
+                                .map(divaConstraintModel -> {
+                                    ApplicationTransactionsDto.TransactionDto dto = new ApplicationTransactionsDto.TransactionDto();
+
+                                    if (divaConstraintModel instanceof DivaEntryMethodModel) {
+                                        DivaEntryMethodModel divaEntryMethodModel = (DivaEntryMethodModel) divaConstraintModel;
+
+                                        dto.methodName = divaEntryMethodModel.getMethodName();
+
+                                        JavaClassModel clz = divaEntryMethodModel.getJavaClass();
+                                        if (clz != null) {
+                                            dto.className = clz.getQualifiedName();
+                                            dto.classFileId = StreamSupport.stream(javaClassService.getJavaSource(clz.getQualifiedName()).spliterator(), false)
+                                                    .map(sourceReportService::getSourceReportForFileModel)
+                                                    .filter(Objects::nonNull)
+                                                    .map(f -> f.getSourceFileModel().getId().toString())
+                                                    .findFirst()
+                                                    .orElse(null);
+                                        }
+                                    }
+
+                                    return dto;
+                                })
+                                .findFirst()
+                                .orElseGet(ApplicationTransactionsDto.TransactionDto::new);
+
+                        transactionDto.stackTraces = divaContextModel.getTransactions().stream()
+                                .flatMap(divaTxModel -> divaTxModel.getOps().stream()
+                                        .map(divaOpModel -> {
+                                            ApplicationTransactionsDto.StackTraceDto dto = new ApplicationTransactionsDto.StackTraceDto();
+                                            if (divaOpModel instanceof DivaSqlOpModel) {
+                                                DivaSqlOpModel op = (DivaSqlOpModel) divaOpModel;
+                                                dto.sql = op.getSql();
+                                            }
+
+                                            dto.lineNumber = divaOpModel.getStackTrace().getLocation().getLineNumber();
+                                            return dto;
+                                        })
+                                )
+                                .collect(Collectors.toList());
+
+
+                        return transactionDto;
+                    })
+                    .collect(Collectors.toList());
+        }
+
+        return result;
+    }
+
+    @Override
+    public Map<String, Object> getById(GraphRewrite event) {
+        return Collections.emptyMap();
+    }
+
+}
