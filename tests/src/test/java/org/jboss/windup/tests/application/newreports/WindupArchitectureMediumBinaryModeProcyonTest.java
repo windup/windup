fb1e992af17a3b030e392a152c9e2500812dafd8
carlosthe19916 <2582866+carlosthe19916@users.noreply.github.com>
2023-02-28 09:58:19 +0100

    Add WindupArchitectureMediumBinaryModeProcyonTest

---
+++ tests/src/test/java/org/jboss/windup/tests/application/newreports/WindupArchitectureMediumBinaryModeProcyonTest.java
@@ -0,0 +1,25 @@
+package org.jboss.windup.tests.application.newreports;
+
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.windup.graph.GraphContext;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.Properties;
+
+@RunWith(Arquillian.class)
+public class WindupArchitectureMediumBinaryModeProcyonTest extends WindupArchitectureMediumBinaryModeTest {
+    @Test
+    @Ignore
+    public void testRunWindupMediumWithProcyon() throws Exception {
+        final String path = "../test-files/Windup1x-javaee-example.war";
+        try (GraphContext context = createGraphContext()) {
+            Properties props = System.getProperties();
+            props.setProperty("windup.decompiler", "Procyon");
+            super.runTest(context, true, path, false);
+            props.remove("windup.decompiler");
+            allDecompiledFilesAreLinked(context);
+        }
+    }
+}
