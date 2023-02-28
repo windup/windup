18b3a3333eb93ae7580a8cda11e7e72fc1eaa191
carlosthe19916 <2582866+carlosthe19916@users.noreply.github.com>
2023-02-28 11:54:58 +0100

    Add WindupArchitectureSourceWithBadPOMTest

---
+++ tests/src/test/java/org/jboss/windup/tests/application/newreports/WindupArchitectureSourceWithBadPOMTest.java
@@ -0,0 +1,48 @@
+package org.jboss.windup.tests.application.newreports;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.forge.arquillian.AddonDependencies;
+import org.jboss.forge.arquillian.AddonDependency;
+import org.jboss.forge.arquillian.archive.AddonArchive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.windup.graph.GraphContext;
+import org.jboss.windup.tests.application.WindupArchitectureTest;
+import org.jboss.windup.tests.application.rules.TestServletAnnotationRuleProvider;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.Collections;
+import java.util.List;
+
+@RunWith(Arquillian.class)
+public class WindupArchitectureSourceWithBadPOMTest extends WindupArchitectureTest {
+    @Deployment
+    @AddonDependencies({
+            @AddonDependency(name = "org.jboss.windup.config:windup-config-xml"),
+            @AddonDependency(name = "org.jboss.windup.graph:windup-graph"),
+            @AddonDependency(name = "org.jboss.windup.exec:windup-exec"),
+            @AddonDependency(name = "org.jboss.windup.rules.apps:windup-rules-java"),
+            @AddonDependency(name = "org.jboss.windup.rules.apps:windup-rules-java-ee"),
+            @AddonDependency(name = "org.jboss.windup.utils:windup-utils"),
+            @AddonDependency(name = "org.jboss.windup.reporting:windup-reporting"),
+            @AddonDependency(name = "org.jboss.windup.reporting:windup-reporting-data"),
+            @AddonDependency(name = "org.jboss.forge.furnace.container:cdi"),
+    })
+    public static AddonArchive getDeployment() {
+        return ShrinkWrap.create(AddonArchive.class)
+                .addBeansXML()
+                .addClass(WindupArchitectureTest.class)
+                .addClass(TestServletAnnotationRuleProvider.class);
+    }
+
+    @Test
+    public void testRunWindupSourceMode() throws Exception {
+        try (GraphContext context = createGraphContext()) {
+            // The test-files folder in the project root dir.
+            List<String> includeList = Collections.emptyList();
+            List<String> excludeList = Collections.emptyList();
+            super.runTest(context, false, "../test-files/src_example_bad_maven", true);
+        }
+    }
+}
