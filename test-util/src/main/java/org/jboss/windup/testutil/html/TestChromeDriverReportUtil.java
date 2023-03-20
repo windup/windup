46c90c861a5923ff0a7502dd8c3e69ec9a30e7d3
Marco Rizzi <mrizzi@users.noreply.github.com>
2023-03-20 08:56:15 +0100

    Fix 'windup-tests' job (#1616)

--- test-util/src/main/java/org/jboss/windup/testutil/html/TestChromeDriverReportUtil.java
+++ test-util/src/main/java/org/jboss/windup/testutil/html/TestChromeDriverReportUtil.java
@@ -1,7 +1,7 @@
 package org.jboss.windup.testutil.html;
 
 import java.nio.file.Path;
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -13,8 +13,15 @@ import org.openqa.selenium.chrome.ChromeOptions;
  */
 public class TestChromeDriverReportUtil extends TestReportUtil {
     public TestChromeDriverReportUtil() {
+        // This is the "right" solution https://www.selenium.dev/blog/2022/using-java11-httpclient/
+        // but it generated a ClassNotFoundException: java.net.http.HttpTimeoutException from JBoss Modules
+        // due to https://issues.redhat.com/browse/MODULES-392 and
+        // org.jboss.forge.furnace:furnace:2.29.1.Final using org.jboss.modules:jboss-modules:1.9.1.Final
+//        System.setProperty("webdriver.http.factory", "jdk-http-client");
+        System.setProperty("webdriver.http.factory", "netty");
         ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--headless");
+        chromeOptions.addArguments("--headless=new");
+        chromeOptions.addArguments("--remote-allow-origins=*");
         this.driver = new ChromeDriver(chromeOptions);
     }
 
@@ -23,7 +30,7 @@ public class TestChromeDriverReportUtil extends TestReportUtil {
         try {
             if (!filePath.toFile().exists())
                 throw new CheckFailedException("Requested page file does not exist: " + filePath);
-            driver.manage().timeouts().setScriptTimeout(30, TimeUnit.SECONDS);
+            driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(30));
             driver.get(filePath.toUri().toString());
         } catch (Exception e) {
             e.printStackTrace();
