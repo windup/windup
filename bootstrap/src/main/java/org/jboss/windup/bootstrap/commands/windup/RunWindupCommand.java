282cf5a318f30ef50a9f6ec0895ebe0340d0ff35
Marco Rizzi <mrizzi@users.noreply.github.com>
2023-03-27 18:32:19 +0200

    WINDUP-3612 non-zero RC when failing an analysis (#1623)
    * WINDUP-3612 non-zero RC when failing an analysis
    
    * WINDUP-3612 Added new 'exitCodes' option
    
    * WINDUP-3612 Removed the 'batch mode' constraint
    
    (cherry picked from commit 8218563bb80d79c543160fddfa1498339f8deb53)

--- bootstrap/src/main/java/org/jboss/windup/bootstrap/commands/windup/RunWindupCommand.java
+++ bootstrap/src/main/java/org/jboss/windup/bootstrap/commands/windup/RunWindupCommand.java
@@ -62,15 +62,19 @@ public class RunWindupCommand implements Command, FurnaceDependent {
     private Furnace furnace;
     private final List<String> arguments;
     private final AtomicBoolean batchMode;
+    private final AtomicBoolean exitCodes;
 
-    public RunWindupCommand(List<String> arguments, AtomicBoolean batchMode) {
+    public RunWindupCommand(List<String> arguments, AtomicBoolean batchMode, AtomicBoolean exitCodes) {
         this.arguments = arguments;
         this.batchMode = batchMode;
+        this.exitCodes = exitCodes;
     }
 
     @Override
     public CommandResult execute() {
-        runWindup(arguments);
+        final int returnCode = runWindup(arguments);
+        // https://issues.redhat.com/browse/WINDUP-3612
+        if (returnCode != 0 && exitCodes.get()) System.exit(returnCode);
         return CommandResult.EXIT;
     }
 
@@ -85,7 +89,7 @@ public class RunWindupCommand implements Command, FurnaceDependent {
     }
 
     @SuppressWarnings("unchecked")
-    private void runWindup(List<String> arguments) {
+    private int runWindup(List<String> arguments) {
         Iterable<ConfigurationOption> optionIterable = WindupConfiguration.getWindupConfigurationOptions(furnace);
         Map<String, ConfigurationOption> options = new HashMap<>();
         for (ConfigurationOption option : optionIterable)
@@ -166,14 +170,12 @@ public class RunWindupCommand implements Command, FurnaceDependent {
 
         // Target - interactive
         Collection<String> targets = (Collection<String>) optionValues.get(TargetOption.NAME);
-        if (targets != null && targets.contains("eap"))
-        {
+        if (targets != null && targets.contains("eap")) {
             System.err.println("ERROR: " + "Version must be specified for target 'eap' (for example, 'eap7' or 'eap8')");
-            return;
+            return 1;
         }
 
-        if ((targets == null || targets.isEmpty()) && !batchMode.get())
-        {
+        if ((targets == null || targets.isEmpty()) && !batchMode.get()) {
             String target = Bootstrap.promptForListItem("Please select a target:", ruleProviderRegistryCache.getAvailableTargetTechnologies(), "eap7");
             targets = Collections.singleton(target);
             optionValues.put(TargetOption.NAME, targets);
@@ -181,7 +183,7 @@ public class RunWindupCommand implements Command, FurnaceDependent {
 
         boolean validationSuccess = validateOptionValues(options, optionValues);
         if (!validationSuccess)
-            return;
+            return 1;
 
         boolean eapTarget = targets.stream().anyMatch(target -> target.startsWith("eap"));
         boolean disableReport = false;
@@ -228,13 +230,13 @@ public class RunWindupCommand implements Command, FurnaceDependent {
         }
 
         if (!validateInputAndOutputPath(windupConfiguration.getInputPaths(), windupConfiguration.getOutputDirectory()))
-            return;
+            return 1;
 
         try {
             windupConfiguration.useDefaultDirectories();
         } catch (IOException e) {
             System.err.println("ERROR: Failed to create default directories due to: " + e.getMessage());
-            return;
+            return 1;
         }
 
         Boolean overwrite = (Boolean) windupConfiguration.getOptionMap().get(OverwriteOption.NAME);
@@ -248,12 +250,11 @@ public class RunWindupCommand implements Command, FurnaceDependent {
             if (!Bootstrap.prompt(promptMsg, false, batchMode.get())) {
                 String outputPath = windupConfiguration.getOutputDirectory().toString();
                 System.err.println("Files exist in " + outputPath + ", but --overwrite not specified. Aborting!");
-                return;
+                return 1;
             }
         }
 
         FileUtils.deleteQuietly(windupConfiguration.getOutputDirectory().toFile());
-        Path graphPath = windupConfiguration.getOutputDirectory().resolve(GraphContextFactory.DEFAULT_GRAPH_SUBDIRECTORY);
 
         System.out.println();
         if (windupConfiguration.getInputPaths().size() == 1) {
@@ -288,9 +289,11 @@ public class RunWindupCommand implements Command, FurnaceDependent {
         } catch (Exception e) {
             System.err.println("Execution failed due to: " + e.getMessage());
             e.printStackTrace();
+            return 1;
+        } finally {
+            Util.deleteGraphDataUnlessInhibited(windupConfiguration, windupConfiguration.getOutputDirectory().resolve(GraphContextFactory.DEFAULT_GRAPH_SUBDIRECTORY));
         }
-
-        Util.deleteGraphDataUnlessInhibited(windupConfiguration, graphPath);
+        return 0;
     }
 
 
